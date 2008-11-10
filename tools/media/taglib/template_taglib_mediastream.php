cbb73eaea566d5eea3a0c13d730cf7c1e9bb9474
Christian Achatz <christian.achatz@adventure-php-framework.org>
2008-11-10 20:15:44 +0000

    Bugfix to the template_taglib_mediastream taglib. Now the tag is transformed. See http://forum.adventure-php-framework.org/de/viewtopic.php?p=342#p342 for more details.

--- tools/media/taglib/template_taglib_mediastream.php
+++ tools/media/taglib/template_taglib_mediastream.php
@@ -9,11 +9,27 @@
    *  @author Christian Achatz
    *  @version
    *  Version 0.1, 01.11.2008<br />
+   *  Version 0.2, 10.11.2008 (Bugfix: tag was not transformed within a template)<br />
    */
    class template_taglib_mediastream extends ui_mediastream
    {
 
       function template_taglib_mediastream(){
+      }
+
+
+      /**
+      *  @public
+      *
+      *  Registers the taglib to be transformed by the parent (template taglib).
+      *
+      *  @author Christian Achatz
+      *  @version
+      *  Version 0.1, 10.11.2008 (Bugfix: see http://forum.adventure-php-framework.org/de/viewtopic.php?p=342#p342)<br />
+      */
+      function onAfterAppend(){
+         $this->__ParentObject->registerTagLibModule(get_class($this));
+       // end function
       }
 
     // end class
