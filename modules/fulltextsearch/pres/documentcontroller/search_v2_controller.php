efd8b78ec7df6f9eedae530b9347f93f9e44f974
Christian Achatz <christian.achatz@adventure-php-framework.org>
2008-11-25 20:54:10 +0000

    Added the license block at the top of each file.

--- modules/fulltextsearch/pres/documentcontroller/search_v2_controller.php
+++ modules/fulltextsearch/pres/documentcontroller/search_v2_controller.php
@@ -1,4 +1,24 @@
 <?php
+   /**
+   *  <!--
+   *  This file is part of the adventure php framework (APF) published under
+   *  http://adventure-php-framework.org.
+   *
+   *  The APF is free software: you can redistribute it and/or modify
+   *  it under the terms of the GNU Lesser General Public License as published
+   *  by the Free Software Foundation, either version 3 of the License, or
+   *  (at your option) any later version.
+   *
+   *  The APF is distributed in the hope that it will be useful,
+   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   *  GNU Lesser General Public License for more details.
+   *
+   *  You should have received a copy of the GNU Lesser General Public License
+   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+   *  -->
+   */
+
    import('modules::fulltextsearch::biz','fulltextsearchManager');
    import('tools::variablen','variablenHandler');
    import('tools::datetime','dateTimeManager');
@@ -6,7 +26,7 @@
 
 
    /**
-   *  @package modules::fulltextsearch::pres
+   *  @namespace modules::fulltextsearch::pres
    *  @module search_v2_controller
    *
    *  Implementiert den DocumentController für das Template "search.html".<br />
