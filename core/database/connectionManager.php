1a45ec2c84a54240d2458118836dd05805a6a8b9
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-01-30 15:02:10 +0000

    Added a check, that the old MySQLHandler cannot be used with the ConnectionManager. Doing so leads to bad connection interference! Details can be taken from http://forum.adventure-php-framework.org/en/viewtopic.php?f=8&t=23.

--- core/database/connectionManager.php
+++ core/database/connectionManager.php
@@ -11,7 +11,7 @@
    *
    *  The APF is distributed in the hope that it will be useful,
    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
    *  GNU Lesser General Public License for more details.
    *
    *  You should have received a copy of the GNU Lesser General Public License
@@ -23,13 +23,10 @@
    *  @namespace core::database
    *  @class connectionManager
    *
-   *  Implementiert eine Fabric für Datenbank-Handler. Muss als ServiceObject erzeugt werden,<br />
-   *  da sonst der Singleton-Cache nicht korrekt benutzt werden kann und die Applikation dann<br />
-   *  i.d.R. unperformant werden!<br />
-   *  Beispiel:<br />
-   *  <br />
-   *  $connMgr = &$this->__getServiceObject('core::database','ConnectionManager');<br />
-   *  $DBHandler = &$connMgr->getConnection('<ConnectionKey>');<br />
+   *  The ConnectionManager is a database connection fabric. To gain performance and to enable
+   *  APF style configuration the manager must be created as a service object. Example:
+   *  <pre>$connMgr = &$this->__getServiceObject('core::database','connectionManager');
+   *  $dBConn = &$connMgr->getConnection('&lt;ConnectionKey&gt;');</pre>
    *
    *  @author Christian Achatz
    *  @version
@@ -53,53 +50,62 @@
       /**
       *  @public
       *
-      *  Gibt einen initialisierten Handler für eine Datenbank-Verbindung zurück.<br />
+      *  Returns the initialized handler for the desired connection key. Caches connections, that
+      *  were created previously.
       *
-      *  @param string $ConnectionKey desired configuration section
-      *  @return object $DatebaseHandler instance of an AbstractDatabaseHandler connection layer
+      *  @param string $connectionKey desired configuration section
+      *  @return AbstractDatabaseHandler $databaseHandler instance of a connection layer
       *
       *  @author Christian Achatz
       *  @version
       *  Version 0.1, 09.11.2007<br />
       *  Version 0.2, 23.02.2008<br />
-      *  Version 0.3, 24.02.2008 (Caching eingeführt; kein ConfigOffset => E_USER_ERROR)<br />
+      *  Version 0.3, 24.02.2008 (Introduced caching)<br />
       *  Version 0.4, 21.06.2008 (Replaced APPS__ENVIRONMENT with a value from the Registry)<br />
-      *  Version 0.5, 05.10.2008 (Bugfix: usage of two or more identical connections (e.g. of type MySQLx) led to interferences. Thus, service object usage was changed (line 84))<br />
+      *  Version 0.5, 05.10.2008 (Bugfix: usage of two or more identical connections (e.g. of type MySQLx) led to interferences. Thus, service object usage was changed)<br />
+      *  Version 0.6, 30.01.2009 (Added a check, that the old MySQLHandler cannot be used with the ConnectionManager. Doing so leads to bad connection interference!)<br />
       */
-      function &getConnection($ConnectionKey){
+      function &getConnection($connectionKey){
 
-         // Prüfen, ob bereits eine Instanz erzeugt wurde
-         $ConnectionHash = md5($ConnectionKey);
+         // check, if connection was already created
+         $ConnectionHash = md5($connectionKey);
 
          if(isset($this->__Connections[$ConnectionHash])){
             return $this->__Connections[$ConnectionHash];
           // end if
          }
 
-         // Konfiguration lesen
+         // read configuration
          $Config = &$this->__getConfiguration('core::database','connections');
 
-         // Sektion gemäß ConnectionKey holen
-         $Section = $Config->getSection($ConnectionKey);
+         // get config section
+         $Section = $Config->getSection($connectionKey);
 
          if($Section == null){
             $Reg = &Singleton::getInstance('Registry');
             $Environment = $Reg->retrieve('apf::core','Environment');
-            trigger_error('[connectionManager::getConnection()] The given configuration section ("'.$ConnectionKey.'") does not exist in configuration file "'.$Environment.'_connections.ini" in namespace "core::database"!',E_USER_ERROR);
+            trigger_error('[connectionManager::getConnection()] The given configuration section ("'.$connectionKey.'") does not exist in configuration file "'.$Environment.'_connections.ini" in namespace "core::database"!',E_USER_ERROR);
             exit(1);
           // end if
          }
 
-         // Handler einbinden
+         // check, if the handler was "MySQL", because this class is not allowed
+         if(strtolower($Section['DB.Type']) === 'mysql'){
+            trigger_error('[connectionManager::getConnection()] The connection type may not be equal to "MySQL". Please check the connection configuration at the "'.$connectionKey.'" section!',E_USER_ERROR);
+            exit(1);
+          // end if
+         }
+
+         // include the handler
          if(!class_exists($Section['DB.Type'].'Handler')){
             import('core::database',$Section['DB.Type'].'Handler');
           // end if
          }
 
-         // Handler erzeugen und cachen
+         // create the handler
          $this->__Connections[$ConnectionHash] = &$this->__getAndInitServiceObject('core::database',$Section['DB.Type'].'Handler',$Section,'NORMAL');
 
-         // Handler zurückgeben
+         // return the handler
          return $this->__Connections[$ConnectionHash];
 
        // end function
