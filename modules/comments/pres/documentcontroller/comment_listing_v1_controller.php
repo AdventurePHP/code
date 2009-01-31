ceed6c556f590a6dca1cb9c623d82f9569730d29
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-01-31 11:59:10 +0000

    Removed the old bbCodeParser component and replaced it with the new one.

--- modules/comments/pres/documentcontroller/comment_listing_v1_controller.php
+++ modules/comments/pres/documentcontroller/comment_listing_v1_controller.php
@@ -11,7 +11,7 @@
    *
    *  The APF is distributed in the hope that it will be useful,
    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
    *  GNU Lesser General Public License for more details.
    *
    *  You should have received a copy of the GNU Lesser General Public License
@@ -21,7 +21,7 @@
 
    import('modules::comments::pres::documentcontroller','commentBaseController');
    import('tools::datetime','dateTimeManager');
-   import('tools::string','bbCodeParser');
+   import('tools::string','AdvancedBBCodeParser');
    import('tools::link','frontcontrollerLinkHandler');
 
 
@@ -29,13 +29,13 @@
    *  @namespace modules::comments::pres::documentcontroller
    *  @class comment_listing_v1_controller
    *
-   *  Implementiert den DocumentController für ds Template 'listing.html'.<br />
+   *  Implements the document controller for the 'listing.html' template.
    *
    *  @author Christian Achatz
    *  @version
    *  Version 0.1, 22.08.2007<br />
-   *  Version 0.2, 20.04.2008 (AllowedServers angepasst)<br />
-   *  Version 0.3, 12.06.2008 (Anzeige-Quickhack entfernt)<br />
+   *  Version 0.2, 20.04.2008 (Added a display restriction)<br />
+   *  Version 0.3, 12.06.2008 (Removed the display restriction)<br />
    */
    class comment_listing_v1_controller extends commentBaseController
    {
@@ -47,62 +47,65 @@
       /**
       *  @public
       *
-      *  Implementiert die abstrakte Methode "transformContent()".<br />
+      *  Displays the paged comment list.
       *
       *  @author Christian Achatz
       *  @version
       *  Version 0.1, 22.08.2007<br />
-      *  Version 0.2, 02.09.2007 (Funktion auf anderen Hosts deaktiviert, dass bei Demopackages keine Fehler auftreten)<br />
-      *  Version 0.3, 09.03.2008 (Deaktivierung wegen Indexer verändert)<br />
-      *  Version 0.4, 12.06.2008 (Anzeige-Quickhack entfernt)<br />
+      *  Version 0.2, 02.09.2007<br />
+      *  Version 0.3, 09.03.2008 (Changed deactivation due to indexation)<br />
+      *  Version 0.4, 12.06.2008 (Removed display limitation quick hack)<br />
+      *  Version 0.5, 30.01.2009 (Replaced the bbCodeParser with the AdvancedBBCodeParser)<br />
       */
       function transformContent(){
 
-         // Kategorie-Schlüssel laden
+         // load the category key
          $this->__loadCategoryKey();
 
-         // Mapper holen
+         // get the manager
          $M = &$this->__getAndInitServiceObject('modules::comments::biz','commentManager',$this->__CategoryKey);
 
-         // Einträge laden
+         // load the entries
          $Entries = $M->loadEntries();
 
          $Buffer = (string)'';
          $Template__ArticleComment = &$this->__getTemplate('ArticleComment');
-         $bbCP = &$this->__getServiceObject('tools::string','bbCodeParser');
+
+         // init bb code parser (remove some provider, that we don't need configuration files)
+         $bP = &$this->__getServiceObject('tools::string','AdvancedBBCodeParser');
+         $bP->removeProvider('standard.font.color');
+         $bP->removeProvider('standard.font.size');
 
          for($i = 0; $i < count($Entries); $i++){
 
-            // Werte setzen
+            // fill template
             $Template__ArticleComment->setPlaceHolder('Number',$i + 1);
             $Template__ArticleComment->setPlaceHolder('Name',$Entries[$i]->get('Name'));
             $Template__ArticleComment->setPlaceHolder('Date',dateTimeManager::convertDate2Normal($Entries[$i]->get('Date')));
             $Template__ArticleComment->setPlaceHolder('Time',$Entries[$i]->get('Time'));
-            $Template__ArticleComment->setPlaceHolder('Comment',$bbCP->parseText($Entries[$i]->get('Comment')));
+            $Template__ArticleComment->setPlaceHolder('Comment',$bP->parseCode($Entries[$i]->get('Comment')));
 
-            // Template transformieren und zum Puffer hinzufügen
+            // add current item to the buffer
             $Buffer .= $Template__ArticleComment->transformTemplate();
 
           // end for
          }
 
-         // Falls keine Artikel vorliegen, Hinweis anzeigen
+         // display hint, if no entries are to display
          if(count($Entries) < 1){
             $Template__NoEntries = &$this->__getTemplate('NoEntries');
             $Buffer = $Template__NoEntries->transformTemplate();
           // end if
          }
 
-         // Ausgabe in Puffer setzen
+         // display the list
          $this->setPlaceHolder('Content',$Buffer);
 
-         // Pager einsetzen
+         // display the pager
          $this->setPlaceHolder('Pager',$M->getPager('comments'));
 
-         // Hinzufüge-Link erstellen
+         // generate the add comment link
          $URLParameter = $M->getURLParameter();
-
-         // Link per frontcontrollerLinkHandler generieren
          $this->setPlaceHolder(
                                'Link',
                                frontcontrollerLinkHandler::generateLink(
