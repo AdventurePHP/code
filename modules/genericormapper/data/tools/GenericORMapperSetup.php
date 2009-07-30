f7caaf6f68019bdc6ad4f1e92d38cdc29423e744
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-07-30 19:28:14 +0000

    Changed foreign key columns to UNSIGNED. See bugreport http://forum.adventure-php-framework.org/de/viewtopic.php?f=8&t=155 for details.

--- modules/genericormapper/data/tools/GenericORMapperSetup.php
+++ modules/genericormapper/data/tools/GenericORMapperSetup.php
@@ -23,22 +23,23 @@
 
 
    /**
-   *  @namespace modules::genericormapper::data
-   *  @classs
-   *
-   *  Tool to setup the database automatically. Changes to the database layout cannot be applied.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 11.05.2008<br />
-   */
+    * @namespace modules::genericormapper::data
+    * @class GenericORMapperSetup
+    *
+    * Tool to setup the database automatically. <strong>Changes to the database
+    * layout cannot be applied.</strong>
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 11.05.2008<br />
+    */
    class GenericORMapperSetup extends BaseMapper
    {
 
       /**
-      *  @protected
-      *  Indicators for the datatype mapping.
-      */
+       * @protected
+       * Indicators for the datatype mapping.
+       */
       protected $__RowTypeMappingFrom = array(
                                         '/^VARCHAR\(([0-9]+)\)$/i',
                                         '/^TEXT$/i',
@@ -46,9 +47,9 @@
                                        );
 
       /**
-      *  @protected
-      *  Replace strings for the datatype mapping.
-      */
+       * @protected
+       * Replace strings for the datatype mapping.
+       */
       protected $__RowTypeMappingTo = array(
                                       'VARCHAR($1) character set utf8 NOT NULL default \'\'',
                                       'TEXT character set utf8 NOT NULL',
@@ -57,9 +58,9 @@
 
 
       /**
-      *  @protected
-      *  Stores the MySQL storage engine type.
-      */
+       * @protected
+       * Stores the MySQL storage engine type.
+       */
       protected $__StorageEngine = 'MyISAM';
 
 
@@ -68,27 +69,27 @@
 
 
       /**
-      *  @public
-      *
-      *  Setups the database. Uses the connectionManager, hence a valid database connection is required.
-      *  If the third parameter ($ConnectionName) is left blank, the statements are displayed only.
-      *
-      *  @param string $ConfigNamespace namespace, where the desired mapper configuration is located
-      *  @param string $ConfigNameAffix name affix of the object and relation definition files
-      *  @param string $ConnectionName name of the connection, that the mapper should use to acces the database
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 31.05.2008<br />
-      *  Version 0.2, 23.06.2008 (Improvements (configurable storage engine, ...) and addition of display only feature)<br />
-      */
-      function setupDatabase($ConfigNamespace,$ConfigNameAffix,$ConnectionName = null){
+       * @public
+       *
+       * Setups the database. Uses the connectionManager, hence a valid database connection is required.
+       * If the third parameter ($ConnectionName) is left blank, the statements are displayed only.
+       *
+       * @param string $configNamespace namespace, where the desired mapper configuration is located
+       * @param string $configNameAffix name affix of the object and relation definition files
+       * @param string $connectionName name of the connection, that the mapper should use to acces the database
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 31.05.2008<br />
+       * Version 0.2, 23.06.2008 (Improvements (configurable storage engine, ...) and addition of display only feature)<br />
+       */
+      public function setupDatabase($configNamespace,$configNameAffix,$connectionName = null){
 
          // set the config namespace
-         $this->__ConfigNamespace = $ConfigNamespace;
+         $this->__ConfigNamespace = $configNamespace;
 
          // set the config name affix
-         $this->__ConfigNameAffix = $ConfigNameAffix;
+         $this->__ConfigNameAffix = $configNameAffix;
 
          // setup object layout
          $Objects = $this->__generateObjectLayout();
@@ -97,7 +98,7 @@
          $Relations = $this->__generateRelationLayout();
 
          // display only
-         if($ConnectionName === null){
+         if($connectionName === null){
 
             // display object structure
             echo '<pre>';
@@ -121,7 +122,7 @@
             $cM = &$this->__getServiceObject('core::database','connectionManager');
 
             // initialize connection
-            $this->__DBDriver = &$cM->getConnection($ConnectionName);
+            $this->__DBDriver = &$cM->getConnection($connectionName);
 
             // create object structure
             foreach($Objects as $Object){
@@ -143,19 +144,19 @@
 
 
       /**
-      *  @protected
-      *
-      *  Creates the setup statements for the object persistance.
-      *
-      *  @return string Sql setup script
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 11.05.2008<br />
-      *  Version 0.2, 31.05.2008 (Code completed and refactored due to changes on the mapping table)<br />
-      *  Version 0.3, 09.12.2008 (Replaced TINYINT by INT)<br />
-      *  Version 0.4, 18.05.2009 (Changed primary key columns to UNSIGNED)<br />
-      */
+       * @protected
+       *
+       * Creates the setup statements for the object persistance.
+       *
+       * @return string Sql setup script
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 11.05.2008<br />
+       * Version 0.2, 31.05.2008 (Code completed and refactored due to changes on the mapping table)<br />
+       * Version 0.3, 09.12.2008 (Replaced TINYINT by INT)<br />
+       * Version 0.4, 18.05.2009 (Changed primary key columns to UNSIGNED)<br />
+       */
       protected function __generateObjectLayout(){
 
          // create mapping table
@@ -163,19 +164,19 @@
 
          // generate tables for objects
          $setup = array();
-         foreach($this->__MappingTable as $Name => $Attributes){
+         foreach($this->__MappingTable as $name => $attributes){
 
             // header
-            $create = 'CREATE TABLE IF NOT EXISTS `'.$Attributes['Table'].'` ('.PHP_EOL;
+            $create = 'CREATE TABLE IF NOT EXISTS `'.$attributes['Table'].'` ('.PHP_EOL;
 
             // id row
-            $create .= '  `'.$Attributes['ID'].'` INT(5) UNSIGNED NOT NULL auto_increment,'.PHP_EOL;
+            $create .= '  `'.$attributes['ID'].'` INT(5) UNSIGNED NOT NULL auto_increment,'.PHP_EOL;
 
             // object properties
-            foreach($Attributes as $Key => $Value){
-               if($Key != 'ID' && $Key != 'Table'){
-                  $Value = preg_replace($this->__RowTypeMappingFrom,$this->__RowTypeMappingTo,$Value);
-                  $create .= '  `'.$Key.'` '.$Value.','.PHP_EOL;
+            foreach($attributes as $key => $value){
+               if($key != 'ID' && $key != 'Table'){
+                  $value = preg_replace($this->__RowTypeMappingFrom,$this->__RowTypeMappingTo,$value);
+                  $create .= '  `'.$key.'` '.$value.','.PHP_EOL;
                 // end if
                }
              // end if
@@ -186,7 +187,7 @@
             $create .= '  `ModificationTimestamp` timestamp NOT NULL default \'0000-00-00 00:00:00\','.PHP_EOL;
 
             // primary key
-            $create .= '  PRIMARY KEY (`'.$Attributes['ID'].'`)'.PHP_EOL;
+            $create .= '  PRIMARY KEY (`'.$attributes['ID'].'`)'.PHP_EOL;
 
             // footer
             $create .= ') ENGINE='.$this->__StorageEngine.' DEFAULT CHARSET=utf8;';
@@ -204,18 +205,19 @@
 
 
       /**
-      *  @protected
-      *
-      *  Creates the setup statements for the relation persistence.
-      *
-      *  @return string Sql setup script.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 31.05.2008<br />
-      *  Version 0.2, 09.12.2008 (Replaced TINYINT by INT)<br />
-      *  Version 0.3, 18.05.2009 (Changed primary key columns to UNSIGNED)<br />
-      */
+       * @protected
+       *
+       * Creates the setup statements for the relation persistence.
+       *
+       * @return string Sql setup script.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 31.05.2008<br />
+       * Version 0.2, 09.12.2008 (Replaced TINYINT by INT)<br />
+       * Version 0.3, 18.05.2009 (Changed primary key columns to UNSIGNED)<br />
+       * Version 0.4, 30.07.2009 (Changed foreign key columns to UNSIGNED)<br />
+       */
       protected function __generateRelationLayout(){
 
          // create relation table
@@ -223,31 +225,31 @@
 
          // generate tables for objects
          $setup = array();
-         foreach($this->__RelationTable as $Name => $Attributes){
+         foreach($this->__RelationTable as $name => $attributes){
 
             // header
-            $create = 'CREATE TABLE IF NOT EXISTS `'.$Attributes['Table'].'` ('.PHP_EOL;
+            $create = 'CREATE TABLE IF NOT EXISTS `'.$attributes['Table'].'` ('.PHP_EOL;
 
             // id row
-            if($Attributes['Type'] == 'COMPOSITION'){
-               $PKName = 'CMPID';
+            if($attributes['Type'] == 'COMPOSITION'){
+               $pkName = 'CMPID';
              // end if
             }
             else{
-               $PKName = 'ASSID';
+               $pkName = 'ASSID';
              // end if
             }
-            $create .= '  `'.$PKName.'` INT(5) UNSIGNED NOT NULL auto_increment,'.PHP_EOL;
+            $create .= '  `'.$pkName.'` INT(5) UNSIGNED NOT NULL auto_increment,'.PHP_EOL;
 
             // source id
-            $create .= '  `'.$Attributes['SourceID'].'` INT(5) NOT NULL default \'0\','.PHP_EOL;
+            $create .= '  `'.$attributes['SourceID'].'` INT(5) UNSIGNED NOT NULL default \'0\','.PHP_EOL;
 
             // target id
-            $create .= '  `'.$Attributes['TargetID'].'` INT(5) NOT NULL default \'0\','.PHP_EOL;
+            $create .= '  `'.$attributes['TargetID'].'` INT(5) UNSIGNED NOT NULL default \'0\','.PHP_EOL;
 
             // indices
-            $create .= '  PRIMARY KEY  (`'.$PKName.'`),'.PHP_EOL;
-            $create .= '  KEY `JOININDEX` (`'.$Attributes['SourceID'].'`,`'.$Attributes['TargetID'].'`)'.PHP_EOL;
+            $create .= '  PRIMARY KEY  (`'.$pkName.'`),'.PHP_EOL;
+            $create .= '  KEY `JOININDEX` (`'.$attributes['SourceID'].'`,`'.$attributes['TargetID'].'`)'.PHP_EOL;
 
             // footer
             $create .= ') ENGINE='.$this->__StorageEngine.' DEFAULT CHARSET=utf8;';
