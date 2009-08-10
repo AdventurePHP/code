144810a4ad7a1a335a08f83bcc76271e5337577f
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-08-10 20:35:52 +0000

    Changed behaviour of the getValue() method to treat empty values as non existing values. See http://forum.adventure-php-framework.org/de/viewtopic.php?f=1&t=80&start=90#p1012 for mor details.

--- tools/request/RequestHandler.php
+++ tools/request/RequestHandler.php
@@ -20,15 +20,15 @@
    */
 
    /**
-   *  @namespace tools::request
-   *  @class RequestHandler
-   *
-   *  This component let's you easily retrieve values from the request.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 13.12.2008<br />
-   */
+    * @namespace tools::request
+    * @class RequestHandler
+    *
+    * This component let's you easily retrieve values from the request.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 13.12.2008<br />
+    */
    final class RequestHandler
    {
 
@@ -37,24 +37,25 @@
 
 
       /**
-      *  @public
-      *  @static
-      *
-      *  Retrieves the desired content from the request. If the request offset does not exist, the
-      *  given default value is taken. Usage:
-      *  <pre>$value = RequestHandler::getValue('foo','bar');</pre>
-      *
-      *  @param string $name name of the request offset
-      *  @param string $defaultValue the default value
-      *  @return string The desired value.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 13.12.2008<br />
-      */
+       * @public
+       * @static
+       *
+       * Retrieves the desired content from the request. If the request offset does not exist, the
+       * given default value is taken. Usage:
+       * <pre>$value = RequestHandler::getValue('foo','bar');</pre>
+       *
+       * @param string $name name of the request offset
+       * @param string $defaultValue the default value
+       * @return string The desired value.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 13.12.2008<br />
+       * Version 0.2, 10.08.2009 (Empty values are now treated as non existing values as well)<br />
+       */
       static function getValue($name,$defaultValue = null){
 
-         if(isset($_REQUEST[$name])){
+         if(isset($_REQUEST[$name]) && !empty($_REQUEST[$name])){
             $value = $_REQUEST[$name];
           // end if
          }
@@ -70,20 +71,20 @@
 
 
       /**
-      *  @public
-      *  @static
-      *
-      *  Retrieves the desired values from the request. If one request offset does not exist, the
-      *  given default value or null is taken. Usage:
-      *  <pre>$values = RequestHandler::getValues(array('foo' => 'bar','baz'));</pre>
-      *
-      *  @param array $namesWithDefaults an input array with names and default values
-      *  @return array The desired values
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 13.12.2008<br />
-      */
+       * @public
+       * @static
+       *
+       * Retrieves the desired values from the request. If one request offset does not exist, the
+       * given default value or null is taken. Usage:
+       * <pre>$values = RequestHandler::getValues(array('foo' => 'bar','baz'));</pre>
+       *
+       * @param array $namesWithDefaults an input array with names and default values
+       * @return array The desired values
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 13.12.2008<br />
+       */
       static function getValues($namesWithDefaults){
 
          // initialize values
