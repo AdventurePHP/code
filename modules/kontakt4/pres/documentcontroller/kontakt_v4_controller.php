31c7621c10cfb58e607b4de76eedbe8a1dcf02ec
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-09-17 20:29:21 +0000

    Adapted the contact module to the new form taglib structure.

--- modules/kontakt4/pres/documentcontroller/kontakt_v4_controller.php
+++ modules/kontakt4/pres/documentcontroller/kontakt_v4_controller.php
@@ -1,158 +1,124 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
-   import('tools::request','RequestHandler');
-   import('modules::kontakt4::biz','contactManager');
-
+   import('tools::link','frontcontrollerLinkHandler');
 
    /**
-   *  @namespace modules::kontakt4::pres::documentcontroller
-   *  @class kontakt_v4_controller
-   *
-   *  Implementiert die Präsentationsschicht des Kontaktformulars.<br />
-   *
-   *  @author Christian Schäfer
-   *  @version
-   *  Version 0.1, 03.06.2006<br />
-   *  Version 0.2, 04.06.2006<br />
-   *  Version 0.3, 23.02.2007 (Implementierung nach PageController v2)<br />
-   */
-   class kontakt_v4_controller extends baseController
-   {
+    * @namespace modules::kontakt4::pres::documentcontroller
+    * @class kontakt_v4_controller
+    *
+    * Document controller for the form view of the contact module.
+    *
+    * @author Christian Schäfer
+    * @version
+    * Version 0.1, 03.06.2006<br />
+    * Version 0.2, 04.06.2006<br />
+    * Version 0.3, 23.02.2007<br />
+    * Version 0.4, 12.09.2009 (Refactoring due to changes of the form taglibs)<br />
+    */
+   class kontakt_v4_controller extends baseController {
 
-      /**
-      *  @private
-      *  Array von lokalen Variablen.
-      */
-      protected $_LOCALS;
-
-
-      function kontakt_v4_controller(){
-
-         $this->_LOCALS = RequestHandler::getValues(array('Empfaenger',
-                                                                'AbsenderName',
-                                                                'AbsenderAdresse',
-                                                                'Betreff',
-                                                                'Text'
-                                                               )
-                                                         );
-
-       // end function
+      public function kontakt_v4_controller(){
       }
 
-
       /**
-      *  @public
-      *
-      *  Implementiert die abstrakte Methode transformContent() des coreObjects.<br />
-      *
-      *  @author Christian Schäfer
-      *  @version
-      *  Version 0.1, 03.06.2006<br />
-      *  Version 0.2, 04.06.2006<br />
-      *  Version 0.3, 29.03.2007 (Form->isValid wird nun abgefragt, statt nochmal zu validieren!)<br />
-      *  Version 0.4, 27.05.2007 (Status "abgesendet" wird nun per Form->isSent abgefragt!)<br />
-      */
+       * @public
+       *
+       * Displays the form and handles the user input.
+       *
+       * @author Christian Schäfer
+       * @version
+       * Version 0.1, 03.06.2006<br />
+       * Version 0.2, 04.06.2006<br />
+       * Version 0.3, 29.03.2007<br />
+       * Version 0.4, 27.05.2007<br />
+       * Version 0.5, 12.09.2009 (Refactoring due to changes of the form taglibs)<br />
+       */
       function transformContent(){
 
-         // Referenz auf die Form holen
-         $Form = &$this->__getForm('Kontakt');
+         $form = &$this->__getForm('contact');
 
-         if($Form->get('isValid') && $Form->get('isSent')){
+         // generate a generic action url, to be included in various pages
+         $action = frontcontrollerLinkHandler::generateLink($_SERVER['REQUEST_URI'],$_REQUEST);
+         $form->setAction($action);
 
-            // Was wird gesendet?
-            //
-            // - Kontakt-Person-ID (Empfänger-Person der Mail)
-            // - Name
-            // - E-Mail
-            // - Betreff
-            // - Text
+         if($form->isSent() && $form->isValid()){
+
             $oFD = new oFormData();
-            $oFD->set('RecipientID',$this->_LOCALS['Empfaenger']);
-            $oFD->set('SenderName',$this->_LOCALS['AbsenderName']);
-            $oFD->set('SenderEMail',$this->_LOCALS['AbsenderAdresse']);
-            $oFD->set('Subject',$this->_LOCALS['Betreff']);
-            $oFD->set('Text',$this->_LOCALS['Text']);
 
-            // Formular absenden
+            $recipient = &$form->getFormElementByName('Empfaenger');
+            $oFD->set('RecipientID',$recipient->getAttribute('value'));
+
+            $name = &$form->getFormElementByName('AbsenderName');
+            $oFD->set('SenderName',$name->getAttribute('value'));
+
+            $email = &$form->getFormElementByName('AbsenderAdresse');
+            $oFD->set('SenderEMail',$email->getAttribute('value'));
+
+            $subject = &$form->getFormElementByName('Betreff');
+            $oFD->set('Subject',$subject->getAttribute('value'));
+
+            $text = &$form->getFormElementByName('Text');
+            $oFD->set('Text',$text->get('Content'));
+
             $cM = &$this->__getServiceObject('modules::kontakt4::biz','contactManager');
             $cM->sendContactForm($oFD);
 
           // end if
          }
          else{
-            $this->setPlaceHolder('Inhalt',$this->__buildForm());
+
+            // label the button
+            $config = &$this->__getConfiguration('modules::kontakt4','language');
+            $button = &$form->getFormElementByName('send');
+            $button->setAttribute('value',$config->getValue($this->__Language,'form.button'));
+
+            // fill listeners with the language dependent values
+            $senderError = &$form->getFormElementByID('sender-error');
+            $senderError->setPlaceHolder('content',$config->getValue($this->__Language,'form.name.error'));
+
+            $addrError = &$form->getFormElementByID('addr-error');
+            $addrError->setPlaceHolder('content',$config->getValue($this->__Language,'form.email.error'));
+
+            $subjectError = &$form->getFormElementByID('subject-error');
+            $subjectError->setPlaceHolder('content',$config->getValue($this->__Language,'form.subject.error'));
+
+            $textError = &$form->getFormElementByID('text-error');
+            $textError->setPlaceHolder('content',$config->getValue($this->__Language,'form.text.error'));
+
+            // fill recipient list
+            $recipients = & $form->getFormElementByName('Empfaenger');
+
+            $cM = &$this->__getServiceObject('modules::kontakt4::biz','contactManager');
+            $recipientList = $cM->loadRecipients();
+
+            for($i = 0; $i < count($recipientList); $i++){
+               $recipients->addOption($recipientList[$i]->get('Name'),$recipientList[$i]->get('oID'));
+             // end if
+            }
+
+            $form->transformOnPlace();
+
           // end else
          }
-
-       // end function
-      }
-
-
-      /**
-      *  @protected
-      *
-      *  Erzeugt das Kontakt-Formular.<br />
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 03.06.2006<br />
-      *  Version 0.2, 04.06.2006<br />
-      *  Version 0.3, 23.02.2007 (Implementierung nach PageController v2)<br />
-      *  Version 0.4, 28.03.2007 (Generisches Bild in der Validatorgruppe hinzugefügt)<br />
-      *  Version 0.5, 13.12.2007 (Auf Mehrsprachigkeit erweitert)<br />
-      */
-      protected function __buildForm(){
-
-         // Referenz auf die Form holen
-         $Form__Kontakt = &$this->__getForm('Kontakt');
-
-         // Action setzen
-         $Form__Kontakt->setAttribute('action',$_SERVER['REQUEST_URI']);
-
-         // Button beschriften und formatieren
-         $Config = &$this->__getConfiguration('modules::kontakt4','language');
-         $Button = &$Form__Kontakt->getFormElementByName('KontaktSenden');
-         $Button->setAttribute('value',$Config->getValue($this->__Language,'form.button'));
-         $Button->setAttribute('style',$Config->getValue($this->__Language,'form.button.style'));
-
-         // Bild in der ValidatorGroup setzen (Auslesen der formconfig)
-         $Config = &$this->__getConfiguration('tools::form::taglib','formconfig');
-         $ValGroup = &$Form__Kontakt->getFormElementByName('FormValGroup');
-         $ValGroup->setPlaceHolder('WarnImage',$Config->getValue($this->__Language,'Contact.Warning.Image'));
-         $ValGroup->setPlaceHolder('WarnText',$Config->getValue($this->__Language,'Contact.Warning.Text'));
-
-         // Auswahlfeld Person
-         $Recipients = & $Form__Kontakt->getFormElementByName('Empfaenger');
-
-         // RecipientList laden
-         $cM = &$this->__getServiceObject('modules::kontakt4::biz','contactManager');
-         $RecipientList = $cM->loadRecipients();
-
-         for($i = 0; $i < count($RecipientList); $i++){
-            $Recipients->addOption($RecipientList[$i]->get('Name'),$RecipientList[$i]->get('oID'));
-          // end if
-         }
-
-         return $Form__Kontakt->transformForm();
 
        // end function
       }
