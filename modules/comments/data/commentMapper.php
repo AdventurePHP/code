a89ea3802c126e37f22aea0403e700e2dda488d6
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-09-20 18:36:23 +0000

    Removed MySQLHandler from code due to deletion.

--- modules/comments/data/commentMapper.php
+++ modules/comments/data/commentMapper.php
@@ -1,27 +1,25 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    import('modules::comments::biz','ArticleComment');
-   import('core::database','MySQLHandler');
-
 
    /**
     * @namespace modules::comments::date
@@ -33,12 +31,10 @@
     * @version
     * Version 0.1, 22.08.2007
     */
-   class commentMapper extends coreObject
-   {
+   class commentMapper extends coreObject {
 
       function commentMapper(){
       }
-
 
       /**
        * @public
