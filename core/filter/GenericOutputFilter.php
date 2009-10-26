615d5dbb9eef37ccc49be5295e4de0359cc3880e
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-10-26 23:54:58 +0000

    Formatted code.

--- core/filter/GenericOutputFilter.php
+++ core/filter/GenericOutputFilter.php
@@ -1,40 +1,38 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    /**
-   *  @namespace core::filter
-   *  @class GenericOutputFilter
-   *
-   *  Implements the an output filter, that rewrites links within a HTML code.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 11.12.2008<br />
-   */
-   class GenericOutputFilter extends AbstractFilter
-   {
+    * @namespace core::filter
+    * @class GenericOutputFilter
+    *
+    * Implements the an output filter, that rewrites links within a HTML code.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 11.12.2008<br />
+    */
+   class GenericOutputFilter extends AbstractFilter {
 
-      function GenericOutputFilter(){
+      public function GenericOutputFilter(){
       }
-
 
       /**
        * @public
@@ -56,13 +54,11 @@
          if($urlRewriting === true){
             import('core::filter::output','HtmlLinkRewriteFilter');
             $filter = new HtmlLinkRewriteFilter();
-            return $filter->filter($input);
+            $input = $filter->filter($input);
           // end if
          }
-         else{
-            return $input;
-          // end else
-         }
+         
+         return $input;
 
        // end function
       }
