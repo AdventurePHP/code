a04175cc72c499e6840a83cf6cfa37fd62f224e2
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-01-30 22:40:27 +0000

    Switched return value of the SessionManager to null to be consistent with the RequestHandler.

--- core/session/SessionManager.php
+++ core/session/SessionManager.php
@@ -1,23 +1,23 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    /**
     * @package core::session
@@ -125,6 +125,7 @@
        * @version
        * Version 0.1, 08.03.2006<br />
        * Version 0.2, 15.06.2006 (Now false is returned if the data is not present in the session)<br />
+       * Version 0.3, 30.01.2010 (Switched return value to null to be consistent with the RequestHandler)<br />
        */
       public function loadSessionData($attribute){
 
@@ -133,7 +134,7 @@
           // end if
          }
          else{
-            return false;
+            return null;
           // end else
          }
 
@@ -153,6 +154,7 @@
        * @version
        * Version 0.1, 08.03.2006<br />
        * Version 0.2, 15.06.2006 (Now false is returned if the data is not present in the session)<br />
+       * Version 0.3, 30.01.2010 (Switched return value to null to be consistent with the RequestHandler)<br />
        */
       public function loadSessionDataByNamespace($namespace,$attribute){
 
@@ -161,7 +163,7 @@
           // end if
          }
          else{
-            return false;
+            return null;
           // end else
          }
 
