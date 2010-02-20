708ad58997aacb1f13d2cb211826099710523314
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-02-20 16:54:47 +0000

    Improved the output of the default exception handler; added exception type.

--- core/exceptionhandler/DefaultExceptionHandler.php
+++ core/exceptionhandler/DefaultExceptionHandler.php
@@ -1,83 +1,84 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    import('core::exceptionhandler','AbstractExceptionHandler');
    import('core::logging','Logger');
 
-
    /**
-   *  @package core::exceptionhandler
-   *  @class DefaultExceptionHandler
-   *
-   *  Implements the default APF exception handler for uncaught exceptions.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 21.02.2009<br />
-   */
-   class DefaultExceptionHandler extends AbstractExceptionHandler
-   {
+    * @package core::exceptionhandler
+    * @class DefaultExceptionHandler
+    *
+    * Implements the default APF exception handler for uncaught exceptions.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 21.02.2009<br />
+    */
+   class DefaultExceptionHandler extends AbstractExceptionHandler {
 
       /**
-      *  The number of the exception.
-      */
+       * @var int The number of the exception.
+       */
       protected $__ExceptionNumber = null;
 
       /**
-      *  The message of the exception.
-      */
+       * @var string The message of the exception.
+       */
       protected $__ExceptionMessage = null;
 
       /**
-      *  The file, the exception occures in.
-      */
+       * @var string The file, the exception occures in.
+       */
       protected $__ExceptionFile = null;
 
       /**
-      *  The line, the exception occures in
-      */
+       * @var int The line, the exception occures in
+       */
       protected $__ExceptionLine = null;
 
       /**
-      *  The exception trace.
-      */
-      protected $__ExceptionTrace = array();
+       * @var string The exception type (name of the class).
+       */
+      protected $__ExceptionType = null;
 
+      /**
+       * @var string[] The exception trace.
+       */
+      protected $__ExceptionTrace = array();
 
       public function DefaultExceptionHandler(){
       }
 
-
       /**
-      *  @public
-      *
-      *  Implements the exception handling function, that is called by the APF exception handling
-      *  function.
-      *
-      *  @param Exception $exception the thrown exception
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 21.02.2009<br />
-      */
+       * @public
+       *
+       * Implements the exception handling function, that is called by the APF exception handling
+       * function.
+       *
+       * @param Exception $exception the thrown exception.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 21.02.2009<br />
+       */
       public function handleException($exception){
 
          // fill attributes
@@ -86,6 +87,7 @@
          $this->__ExceptionFile = $exception->getFile();
          $this->__ExceptionLine = $exception->getLine();
          $this->__ExceptionTrace = $exception->getTrace();
+         $this->__ExceptionType = get_class($exception);
 
          // log exception
          $this->__logException();
@@ -96,16 +98,15 @@
        // end function
       }
 
-
       /**
-      *  @private
-      *
-      *  Creates a log entry containing the exception occured.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 21.02.2009<br />
-      */
+       * @private
+       *
+       * Creates a log entry containing the exception occured.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 21.02.2009<br />
+       */
       protected function __logException(){
          $message = '['.($this->__generateExceptionID()).'] '.$this->__ExceptionMessage.' (Number: '.$this->__ExceptionNumber.', File: '.$this->__ExceptionFile.', Line: '.$this->__ExceptionLine.')';
          $L = Singleton::getInstance('Logger');
@@ -113,20 +114,19 @@
        // end function
       }
 
-
       /**
-      *  @private
-      *
-      *  Creates the exception page.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 21.02.2009<br />
-      */
+       * @private
+       *
+       * Creates the exception page.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 21.02.2009<br />
+       */
       protected function __buildExeptionPage(){
 
          // create page
-         $stacktrace = new Page('Stacktrace');
+         $stacktrace = new Page();
          $stacktrace->set('Context','core::exceptionhandler');
          $stacktrace->loadDesign('core::exceptionhandler::templates','exceptionpage');
 
@@ -138,6 +138,7 @@
          $doc->setAttribute('file',$this->__ExceptionFile);
          $doc->setAttribute('line',$this->__ExceptionLine);
          $doc->setAttribute('trace',array_reverse($this->__ExceptionTrace));
+         $doc->setAttribute('type',$this->__ExceptionType);
 
          // create exception page
          return $stacktrace->transform();
@@ -145,16 +146,15 @@
        // end function
       }
 
-
       /**
-      *  @private
-      *
-      *  Generates the exception id.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 21.02.2009<br />
-      */
+       * @private
+       *
+       * Generates the exception id.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 21.02.2009<br />
+       */
       protected function __generateExceptionID(){
          return md5($this->__ExceptionMessage.$this->__ExceptionNumber.$this->__ExceptionFile.$this->__ExceptionLine);
        // end function
--- core/exceptionhandler/documentcontroller/exceptionpage_controller.php
+++ core/exceptionhandler/documentcontroller/exceptionpage_controller.php
@@ -1,88 +1,86 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    /**
-   *  @package core::exceptionhandler::documentcontroller
-   *  @class exceptionpage_controller
-   *
-   *  Implements the exception page's document controller.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 21.02.2009<br />
-   */
-   class exceptionpage_controller extends baseController
-   {
+    * @package core::exceptionhandler::documentcontroller
+    * @class exceptionpage_controller
+    *
+    * Implements the exception page's document controller.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 21.02.2009<br />
+    */
+   class exceptionpage_controller extends baseController {
 
       public function exceptionpage_controller(){
       }
 
-
       /**
-      *  @public
-      *
-      *  Displays the exception page.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 21.02.2009<br />
-      */
+       * @public
+       *
+       * Displays the exception page.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 21.02.2009<br />
+       */
       public function transformContent(){
 
          // get the exception trace, init output buffer
-         $Exceptions = $this->__Attributes['trace'];
+         $exceptions = $this->__Attributes['trace'];
          $buffer = (string)'';
 
          // get template
-         $Template__ExceptionEntry = &$this->__getTemplate('ExceptionEntry');
+         $templateExceptionEntry = &$this->__getTemplate('ExceptionEntry');
 
          // generate stacktrace
-         for($i = 0; $i < count($Exceptions); $i++){
+         for($i = 0; $i < count($exceptions); $i++){
 
-            if(isset($Exceptions[$i]['function'])){
-               $Template__ExceptionEntry->setPlaceHolder('Function',$Exceptions[$i]['function']);
+            if(isset($exceptions[$i]['function'])){
+               $templateExceptionEntry->setPlaceHolder('Function',$exceptions[$i]['function']);
              // end if
             }
 
-            if(isset($Exceptions[$i]['line'])){
-               $Template__ExceptionEntry->setPlaceHolder('Line',$Exceptions[$i]['line']);
+            if(isset($exceptions[$i]['line'])){
+               $templateExceptionEntry->setPlaceHolder('Line',$exceptions[$i]['line']);
              // end if
             }
 
-            if(isset($Exceptions[$i]['file'])){
-               $Template__ExceptionEntry->setPlaceHolder('File',$Exceptions[$i]['file']);
+            if(isset($exceptions[$i]['file'])){
+               $templateExceptionEntry->setPlaceHolder('File',$exceptions[$i]['file']);
              // end if
             }
 
-            if(isset($Exceptions[$i]['class'])){
-               $Template__ExceptionEntry->setPlaceHolder('Class',$Exceptions[$i]['class']);
+            if(isset($exceptions[$i]['class'])){
+               $templateExceptionEntry->setPlaceHolder('Class',$exceptions[$i]['class']);
              // end if
             }
 
-            if(isset($Exceptions[$i]['type'])){
-               $Template__ExceptionEntry->setPlaceHolder('Type',$Exceptions[$i]['type']);
+            if(isset($exceptions[$i]['type'])){
+               $templateExceptionEntry->setPlaceHolder('Type',$exceptions[$i]['type']);
              // end if
             }
 
-            $buffer .= $Template__ExceptionEntry->transformTemplate();
+            $buffer .= $templateExceptionEntry->transformTemplate();
 
           // end for
          }
@@ -90,6 +88,7 @@
          $this->setPlaceHolder('Stacktrace',$buffer);
 
          $this->setPlaceHolder('ExceptionID',$this->__Attributes['id']);
+         $this->setPlaceHolder('ExceptionType',$this->__Attributes['type']);
          $this->setPlaceHolder('ExceptionMessage',$this->__Attributes['message']);
          $this->setPlaceHolder('ExceptionNumber',$this->__Attributes['number']);
          $this->setPlaceHolder('ExceptionFile',$this->__Attributes['file']);
