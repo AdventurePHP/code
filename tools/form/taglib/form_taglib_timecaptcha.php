9980228639f19a2289b90b108974f89da5c0687d
Ralf Schubert <ralf.schubert@adventure-php-framework.org>
2010-03-15 19:56:27 +0000

    Added TimeCaptcha taglib and TimeCaptchaValidator, which can help defeating spam bots in forms.

---
+++ tools/form/taglib/form_taglib_timecaptcha.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *  <!--
+ *  This file is part of the adventure php framework (APF) published under
+ *  http://adventure-php-framework.org.
+ *
+ *  The APF is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The APF is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ *  -->
+ */
+
+import('core::session','SessionManager');
+
+/**
+ * @package tools::form::taglib
+ * @class form_taglib_timecaptcha
+ *
+ * Implements, in combination with TimeCaptchaValidator, a non-visual captcha
+ * which tries to identify bots by the time they need to fill in the data in a
+ * form.
+ *
+ * @author Ralf Schubert
+ * @version
+ * Version 0.1, 14.03.2010<br />
+ */
+class form_taglib_timecaptcha extends form_control {
+
+    /**
+     * @public
+     *
+     * Overwrites the parent's method, because there is nothing to do here.
+     *
+     * @author Ralf Schubert
+     * @version
+     * Version 0.1, 14.03.2010<br />
+     */
+    public function onParseTime() {
+    }
+
+    /**
+     * @public
+     *
+     * Overwrites the parent's method, because there is nothing to do here.
+     *
+     * @author Ralf Schubert
+     * @version
+     * Version 0.1, 14.03.2010<br />
+     */
+    public function onAfterAppend() {
+    }
+
+    /**
+     * @public
+     *
+     * Saves an unix_timestamp in session, which can be used from TimeCaptchaValidator.
+     *
+     * @return string Empty string, because we need no html.
+     *
+     * @author Ralf Schubert
+     * @version
+     * Version 0.1, 14.03.2010<br />
+     */
+    public function transform() {
+        $sessMgr = new SessionManager('tools::form::taglib::form_taglib_timecaptcha');
+        
+        // Delete every stored time in session, which is older than 40 minutes, in order to clean the session.
+        $sessionStore = $sessMgr->getEntryDataKeys();
+        foreach($sessionStore as $sessKey){
+            if($sessMgr->loadSessionData($sessKey) <= (time()-2400)){
+                $sessMgr->deleteSessionData($sessKey);
+            }
+        }
+
+        // Save the new time in session.
+        $sessMgr->saveSessionData('form_'.$this->__ParentObject->getAttribute('name'),time());
+        
+        return '';
+        // end function
+    }
+    
+// end class
+}
+?>
---
+++ tools/form/validator/TimeCaptchaValidator.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *  <!--
+ *  This file is part of the adventure php framework (APF) published under
+ *  http://adventure-php-framework.org.
+ *
+ *  The APF is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The APF is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ *  -->
+ */
+
+import('core::session','SessionManager');
+import('tools::form::validator','TextFieldValidator');
+
+/**
+ * @package tools::form::validator
+ * @class TimeCaptchaValidator
+ *
+ * Implements, in combination with form_taglib_timecaptcha, a non-visual captcha
+ * which tries to identify bots by the time they need to fill in the data in a
+ * form.
+ * The time (in seconds) before the tag becomes valid can be specified in the
+ * target form:timecaptcha element whith the attribute <em>seconds</em>.
+ * Default is 2 seconds.
+ *
+ * @author Ralf Schubert
+ * @version
+ * Version 0.1, 14.03.2010<br />
+ */
+class TimeCaptchaValidator extends TextFieldValidator {
+    /**
+     * @var int The default time in seconds before form gets valid.
+     */
+    protected $__DefaultSeconds = 2;
+
+    /**
+     * @public
+     *
+     * Re-implements the <em>validate()</em> method for the TimeCaptchaValidator
+     *
+     * @param string Empty string, because we have no html-definition
+     * @return boolean true, in case the control to validate is considered valid, false otherwise.
+     *
+     * @author Ralf Schubert
+     * @version
+     * Version 0.1, 14.03.2010<br />
+     */
+    public function validate($input) {
+        $seconds = $this->__Control->getAttribute('seconds');
+        if($seconds === null) {
+            $seconds = $this->__DefaultSeconds;
+        }
+
+        $sessMgr = new SessionManager('tools::form::taglib::form_taglib_timecaptcha');
+        $sessTime = $sessMgr->loadSessionData('form_'.$this->__Control->__ParentObject->getAttribute('name'));
+        $sessMgr->deleteSessionData('form__'.$this->__Control->__ParentObject->getAttribute('name'));
+        unset($sessMgr);
+
+        // If there is no stored time in session control is not valid.
+        if($sessTime === null) {
+            return false;
+        }
+        // Form was sent too fast.
+        if(($sessTime+$seconds) >= time()) {
+            return false;
+        }
+        return true;
+
+        // end function
+    }
+// end class
+}
+?>
