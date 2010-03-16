2b7423d85e13fac59f7435be208805ffaddea732
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-03-16 22:14:52 +0000

    Bugfix 299: refactored the GORM factory to not respect the service type from outside. This led to wrong initialization of the GORM. See http://forum.adventure-php-framework.org/de/viewtopic.php?f=8&t=299&p=2558#p2558 for details.

--- modules/genericormapper/data/GenericORMapperFactory.php
+++ modules/genericormapper/data/GenericORMapperFactory.php
@@ -25,14 +25,25 @@
     * @package modules::genericormapper::data
     * @class GenericORMapperFactory
     *
-    * Implements the factory for the GenericORRelationMapper. Please do only unse the factory in
-    * singleton mode. To achieve this, use the APFObject's <em>__getServiceObject()</em> method.
+    * Implements a factory for creating GenericORRelationMapper instances. Automatically
+    * configures the desired instance using the given params.
+    * <p/>
+    * Please note, that the instanciation method of the factory decides the service mode of the
+    * mapper. This means, that you have to create the factory in NORMAL mode, if you have decided
+    * to use per-use-instanciation, take SINGLETON (default) if you like to have one instance per
+    * request and choose SESSIONSINGLETON in case the mapper should initialize it's mapping and
+    * relation table only once per session.
+    * <p/>
+    * Further, the factory must be created using the service manager. Sample:
+    * <pre>$gormFact = &$this->__getServiceObject('modules::genericormapper::data','GenericORMapperFactory');
+    * $gormFact = &$this->__getServiceObject('modules::genericormapper::data','GenericORMapperFactory','NORMAL');
+    * $gormFact = &$this->__getServiceObject('modules::genericormapper::data','GenericORMapperFactory','SESSIONSINGLETON');</pre>
     *
     * @author Christian Achatz
     * @version
     * Version 0.1, 23.06.2008<br />
     */
-   class GenericORMapperFactory extends APFObject {
+   final class GenericORMapperFactory extends APFObject {
 
       /**
        * @private
@@ -46,14 +57,17 @@
       /**
        * @public
        *
-       * Factory for the generic or mapper module. Returns a reference on an instance of the
-       * GenericORRelationMapper. Do not use the $debugMode option set to true in production
+       * Factory for the generic or mapper. Returns a reference on an instance of the
+       * GenericORRelationMapper class. Do not use the $debugMode option set to true in production
        * environment!
+       * <p/>
+       * As of 1.12, this method has to be used as follows:
+       * <pre>$gormFact = &$this->__getServiceObject('modules::genericormapper::data','GenericORMapperFactory');
+       * $orm = &$gormFact->getGenericORMapper('my::namespace','config_sub_key','db_connection'[,true|false]);</pre>
        *
        * @param string $configNamespace namespace, where the desired mapper configuration is located
        * @param string $configNameAffix name affix of the object and relation definition files
        * @param string $connectionName name of the connection, that the mapper should use to acces the database
-       * @param string $type indicates, if the mapper should be created singleton or session singleton
        * @param boolean $debugMode Inicates, if the generated statements should be logged to a file. Do not use this option set to true in production environment!
        * @return GenericORRelationMapper The desired instance of the generic OR mapper.
        *
@@ -63,11 +77,12 @@
        * Version 0.2, 25.10.2008 (Bugfix: added the $Type parameter to the service manager call)<br />
        * Version 0.3, 26.10.2008 (Bugfix: cache key now recognizes the creation type)<br />
        * Version 0.4, 03.05.2009 (Added the $debugMode param)<br />
+       * Version 0.5, 16.03.2010 (Bugfix 299: removed the type to prevent identical mapper objects)<br />
        */
-      function &getGenericORMapper($configNamespace,$configNameAffix,$connectionName,$type = 'SINGLETON',$debugMode = false){
+      public function &getGenericORMapper($configNamespace,$configNameAffix,$connectionName,$debugMode = false){
 
          // calculate cache key
-         $cacheKey = md5($configNamespace.$configNameAffix.$connectionName.$type);
+         $cacheKey = md5($configNamespace.$configNameAffix.$connectionName);
 
          // create and initaialize a mapper instance
          if(!isset($this->__ORMapperCache[$cacheKey])){
@@ -80,7 +95,7 @@
                      'ConnectionName' => $connectionName,
                      'LogStatements' => $debugMode
                   ),
-                  $type);
+                  'NORMAL');
           // end if
          }
 
