cf6c23894dcb8e953749608581fd39fa680d4349
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-08-13 17:09:06 +0000

    1.13 backport: corrected registry addressing.

--- extensions/htmlheader/biz/PackageNode.php
+++ extensions/htmlheader/biz/PackageNode.php
@@ -1,73 +1,73 @@
 <?php
 
-import('extensions::htmlheader::biz','HtmlNode');
+import('extensions::htmlheader::biz', 'HtmlNode');
+
 /**
  * Description of PackageNode
  *
  * @author Ralf Schubert
  */
 class PackageNode extends HtmlNode {
-    protected $__url = null;
-    protected $__name = null;
-    protected $__type = null;
-    protected $__rewriting = null;
 
-    public function PackageNode($url, $name, $type, $rewriting) {
-        $this->__url = $url;
-        $this->__name = $name;
-        $this->__type = $type;
-        $this->__rewriting = $rewriting;
-        $this->__checksum = md5($url . $name . $type);
-    }
+   protected $__url = null;
+   protected $__name = null;
+   protected $__type = null;
+   protected $__rewriting = null;
 
-    public function transform() {
-        $link = $this->__buildPackageLink(
-                $this->__url,
-                $this->__name,
-                $this->__type,
-                $this->__rewriting
-        );
-        if($this->__type === 'js'){
-            return '<script src="' . $link . '" type="text/javascript"></script>' . PHP_EOL;
-        }
-        return '<link href="' . $link . '" rel="stylesheet" type="text/css" />' . PHP_EOL;
-    }
+   public function PackageNode($url, $name, $type, $rewriting) {
+      $this->__url = $url;
+      $this->__name = $name;
+      $this->__type = $type;
+      $this->__rewriting = $rewriting;
+      $this->__checksum = md5($url . $name . $type);
+   }
 
-    protected function __buildPackageLink($url, $name, $type, $rewriting) {
-        $reg = &Singleton::getInstance('Registry');
+   public function transform() {
+      $link = $this->__buildPackageLink(
+                      $this->__url,
+                      $this->__name,
+                      $this->__type,
+                      $this->__rewriting
+      );
+      if ($this->__type === 'js') {
+         return '<script src="' . $link . '" type="text/javascript"></script>' . PHP_EOL;
+      }
+      return '<link href="' . $link . '" rel="stylesheet" type="text/css" />' . PHP_EOL;
+   }
 
-        if($rewriting === null) {
-            $rewriting = $reg->retrieve('apf::core','URLRewriting');
-        }
+   protected function __buildPackageLink($url, $name, $type, $rewriting) {
 
-        // Generate url if not given
-        if($url === null) {
-            if($rewriting) {
-                $url = $reg->retrieve('apf::core','URLBasePath');
-            }
-            else {
-                $tmpPath = str_replace($_SERVER['DOCUMENT_ROOT'],'', $_SERVER['SCRIPT_FILENAME']);
-                $slash =  (substr($tmpPath, 0,1) !== '/') ? '/' : '';
-                $url = 'http://' . $_SERVER['HTTP_HOST'] . $slash . $tmpPath;
-            }
-            // end if
-        }
+      if ($rewriting === null) {
+         $rewriting = Registry::retrieve('apf::core', 'URLRewriting');
+      }
 
-        if($rewriting) {
-            $actionParam = array(
-                    'extensions_jscsspackager_biz-action/jcp' => 'package/'.$name.'.' . $type
-            );
-            // end if
-        }
-        else {
-            $actionParam = array(
-                    'extensions_jscsspackager_biz-action:jcp' => 'package:'.$name.'.' . $type
-            );
-            // end else
-        }
+      // Generate url if not given
+      if ($url === null) {
+         if ($rewriting) {
+            $url = Registry::retrieve('apf::core', 'URLBasePath');
+         } else {
+            $tmpPath = str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']);
+            $slash = (substr($tmpPath, 0, 1) !== '/') ? '/' : '';
+            $url = 'http://' . $_SERVER['HTTP_HOST'] . $slash . $tmpPath;
+         }
+         // end if
+      }
 
-        // return url
-        return FrontcontrollerLinkHandler::generateLink($url,$actionParam);
-    }
+      if ($rewriting) {
+         $actionParam = array(
+             'extensions_jscsspackager_biz-action/jcp' => 'package/' . $name . '.' . $type
+         );
+         // end if
+      } else {
+         $actionParam = array(
+             'extensions_jscsspackager_biz-action:jcp' => 'package:' . $name . '.' . $type
+         );
+         // end else
+      }
+
+      // return url
+      return FrontcontrollerLinkHandler::generateLink($url, $actionParam);
+   }
+
 }
 ?>
