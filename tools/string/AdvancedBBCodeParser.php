4ed28f7219c46a892cba3924dd1c6a4607ee8dc3
Christian Achatz <christian.achatz@adventure-php-framework.org>
2010-09-06 21:22:00 +0000

    Improved code style and formatting.

--- tools/string/AdvancedBBCodeParser.php
+++ tools/string/AdvancedBBCodeParser.php
@@ -1,127 +1,132 @@
 <?php
    /**
-   *  <!--
-   *  This file is part of the adventure php framework (APF) published under
-   *  http://adventure-php-framework.org.
-   *
-   *  The APF is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published
-   *  by the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  The APF is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-   *  -->
-   */
+    * <!--
+    * This file is part of the adventure php framework (APF) published under
+    * http://adventure-php-framework.org.
+    *
+    * The APF is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published
+    * by the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The APF is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+    * -->
+    */
 
    /**
-   *  @class BBCodeParserDefinition
-   *
-   *  Internal representation of an BBCodeParserProvider. Nearly the same as the TagLib class for
-   *  each Document class.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 28.10.2008
-   */
+    * @package tools::string
+    * @class BBCodeParserDefinition
+    *
+    * Internal representation of an BBCodeParserProvider. Nearly the same as the TagLib class for
+    * each Document class.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 28.10.2008
+    */
    final class BBCodeParserDefinition extends APFObject {
 
       /**
-      *  @protected
-      *  Represents the namespace of the parser provider.
-      */
-      protected $__Namespace;
+       * @private
+       * @var string Represents the namespace of the parser provider.
+       */
+      private $namespace;
 
       /**
-      *  @protected
-      *  Represents the class name of the parser provider an thus the file name (without extentsion!).
-      */
-      protected $__Class;
+       * @private
+       * @var string Represents the class name of the parser provider an thus the file name
+       * (without extentsion!).
+       */
+      private $class;
 
       /**
-      *  @public
-      *
-      *  Initializes the BBCodeParser definition.
-      *
-      *  @param string $namespace the namespace of the parser
-      *  @param string $class the class name of the parser
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      function BBCodeParserDefinition($namespace,$class){
-         $this->__Namespace = $namespace;
-         $this->__Class = $class;
-       // end function
+       * @public
+       *
+       * Initializes the BBCodeParser definition.
+       *
+       * @param string $namespace the namespace of the parser
+       * @param string $class the class name of the parser
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+      public function __construct($namespace,$class){
+         $this->namespace = $namespace;
+         $this->class = $class;
+      }
+
+      public function getNamespace() {
+         return $this->namespace;
+      }
+
+      public function getClass() {
+         return $this->class;
       }
 
     // end class
    }
 
    /**
-   *  @abstract
-   *  @class BBCodeParserProvider
-   *
-   *  The interface for all concrete implementations of the parser provider.
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 28.10.2008
-   */
-   abstract class BBCodeParserProvider extends APFObject
-   {
-
-      function BBCodeParserProvider(){
-      }
-
+    * @abstract
+    * @package tools::string
+    * @class BBCodeParserProvider
+    *
+    * The interface for all concrete implementations of the parser provider.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 28.10.2008
+    */
+   abstract class BBCodeParserProvider extends APFObject {
 
       /**
-      *  @public
-      *  @abstract
-      *
-      *  Interface method for each BBCodeParserProvider, that is called by the AdvancedBBCodeParser.
-      *
-      *  @param string $string the content to parse
-      *  @return string $parsedString the parsed contebt
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      function getOutput($string){
-      }
+       * @public
+       * @abstract
+       *
+       * Interface method for each BBCodeParserProvider, that is called by the AdvancedBBCodeParser.
+       *
+       * @param string $string the content to parse
+       * @return string $parsedString the parsed contebt
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+      public abstract function getOutput($string);
 
     // end class
    }
 
    /**
-   *  @class AdvancedBBCodeParser
-   *
-   *  Implements the advanced bbcode parser for the adventure php framework.
-   *  Usage:
-   *  <pre>$bP = &$this->__getServiceObject('tools::string','AdvancedBBCodeParser');
-   *  [$bP->addProvider(...,...);]
-   *  [$bP->removeProvider(...);]
-   *  $string = '...';
-   *  echo $bP->parseCode($string);</pre>
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 28.10.2008
-   */
+    * @package tools::string
+    * @class AdvancedBBCodeParser
+    *
+    * Implements the advanced bbcode parser for the adventure php framework.
+    * Usage:
+    * <pre>$bP = &$this->__getServiceObject('tools::string','AdvancedBBCodeParser');
+    * [$bP->addProvider(...,...);]
+    * [$bP->removeProvider(...);]
+    * $string = '...';
+    * echo $bP->parseCode($string);</pre>
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 28.10.2008
+    */
    class AdvancedBBCodeParser extends APFObject {
 
       /**
       *  @private
       *  Represents the list of provider.
       */
-      private $__Provider = array();
+      private $provider = array();
 
       /**
       *  @public
@@ -133,75 +138,72 @@
       *  @version
       *  Version 0.1, 28.10.2008
       */
-      function AdvancedBBCodeParser(){
-         $this->__Provider['standard.font.style'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontStyleProvider');
-         $this->__Provider['standard.font.size'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontSizeProvider');
-         $this->__Provider['standard.font.color'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontColorProvider');
-         $this->__Provider['standard.newline'] = new BBCodeParserDefinition('tools::string::bbcpprovider','NewLineProvider');
-       // end function
+      public function __construct(){
+         $this->provider['standard.font.style'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontStyleProvider');
+         $this->provider['standard.font.size'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontSizeProvider');
+         $this->provider['standard.font.color'] = new BBCodeParserDefinition('tools::string::bbcpprovider','FontColorProvider');
+         $this->provider['standard.newline'] = new BBCodeParserDefinition('tools::string::bbcpprovider','NewLineProvider');
       }
 
       /**
-      *  @public
-      *
-      *  This method enables you to add/modify a parsing provider. If a provider already exists with
-      *  the desired name it will be overwritten.
-      *
-      *  @param string $name short name of the provider, that is intended to address it
-      *  @param BBCodeParser $provider a provider representation object
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      function addProvider($name,$provider){
-         $this->__Provider[$name] = $provider;
-       // end function
+       * @public
+       *
+       * This method enables you to add/modify a parsing provider. If a provider already exists with
+       * the desired name it will be overwritten.
+       *
+       * @param string $name short name of the provider, that is intended to address it
+       * @param BBCodeParser $provider a provider representation object
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+      public function addProvider($name,$provider){
+         $this->provider[$name] = $provider;
       }
 
       /**
-      *  @public
-      *
-      *  This method removes a parsing provider.
-      *
-      *  @param string $name short name of the provider, that is intended to address it
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      function removeProvider($name){
-         unset($this->__Provider[$name]);
-       // end function
+       * @public
+       *
+       * This method removes a parsing provider.
+       *
+       * @param string $name short name of the provider, that is intended to address it
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+      public function removeProvider($name){
+         unset($this->provider[$name]);
       }
 
       /**
-      *  @private
-      *
-      *  Sets up the provider to be ready for use. Convertes each BBCodeParserDefinition into
-      *  it's real implementation. If a provider was added / removed the converter is going to
-      *  notive this fact.
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      private function __setUpProvider(){
+       * @private
+       *
+       * Sets up the provider to be ready for use. Convertes each BBCodeParserDefinition into
+       * it's real implementation. If a provider was added / removed the converter is going to
+       * notive this fact.
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+      private function setUpProvider(){
 
-         $T = &Singleton::getInstance('BenchmarkTimer');
-         $T->start('AdvancedBBCodeParser::__setUpProvider()');
+         $t = &Singleton::getInstance('BenchmarkTimer');
+         $t->start('AdvancedBBCodeParser::__setUpProvider()');
 
-         foreach($this->__Provider as $providerName => $DUMMY){
+         foreach($this->provider as $providerName => $DUMMY){
 
-            if(get_class($this->__Provider[$providerName]) == 'BBCodeParserDefinition'){
+            if(get_class($this->provider[$providerName]) == 'BBCodeParserDefinition'){
 
                // include class
-               $namespace = $this->__Provider[$providerName]->get('Namespace');
-               $class = $this->__Provider[$providerName]->get('Class');
+               $namespace = $this->provider[$providerName]->getNamespace();
+               $class = $this->provider[$providerName]->getClass();
                import($namespace,$class);
 
                // convert definition and initialize provider
-               $this->__Provider[$providerName] = &$this->__getServiceObject($namespace,$class,'NORMAL');
+               $this->provider[$providerName] = &$this->__getServiceObject($namespace,$class,'NORMAL');
 
              // end if
             }
@@ -209,42 +211,40 @@
           // end foreach
          }
 
-         $T->stop('AdvancedBBCodeParser::__setUpProvider()');
+         $t->stop('AdvancedBBCodeParser::__setUpProvider()');
 
        // end function
       }
 
       /**
-      *  @public
-      *
-      *  Parses the given string using the provider configured before.
-      *
-      *  @param string $string the string, that should be parsed
-      *  @return string $parsedString the parsed string
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 28.10.2008
-      */
-      function parseCode($string){
+       * @public
+       *
+       * Parses the given string using the provider configured before.
+       *
+       * @param string $string the string, that should be parsed
+       * @return string $parsedString the parsed string
+       *
+       * @author Christian Achatz
+       * @version
+       * Version 0.1, 28.10.2008
+       */
+     public function parseCode($string){
 
-         // start timer
-         $T = &Singleton::getInstance('BenchmarkTimer');
-         $T->start('AdvancedBBCodeParser::parseCode()');
+         $t = &Singleton::getInstance('BenchmarkTimer');
+         $t->start('AdvancedBBCodeParser::parseCode()');
 
          // set up the desired parser
-         $this->__setUpProvider();
+         $this->setUpProvider();
 
          // parse string
-         foreach($this->__Provider as $providerName => $DUMMY){
-            $string = $this->__Provider[$providerName]->getOutput($string);
+         foreach($this->provider as $providerName => $DUMMY){
+            $string = $this->provider[$providerName]->getOutput($string);
           // end function
          }
 
          // stop timer
-         $T->stop('AdvancedBBCodeParser::parseCode()');
+         $t->stop('AdvancedBBCodeParser::parseCode()');
 
-         // return parsed string
          return $string;
 
        // end function
