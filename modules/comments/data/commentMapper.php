b9f74cad17039f66ee04ef69e04ddfedfaff08d6
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-02-20 21:33:57 +0000

    Added value escaping to avoid SQL injection.

--- modules/comments/data/commentMapper.php
+++ modules/comments/data/commentMapper.php
@@ -1,159 +1,131 @@
 <?php
-   /**
-    * <!--
-    * This file is part of the adventure php framework (APF) published under
-    * http://adventure-php-framework.org.
-    *
-    * The APF is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU Lesser General Public License as published
-    * by the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
-    *
-    * The APF is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    * GNU Lesser General Public License for more details.
-    *
-    * You should have received a copy of the GNU Lesser General Public License
-    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-    * -->
-    */
+/**
+ * <!--
+ * This file is part of the adventure php framework (APF) published under
+ * http://adventure-php-framework.org.
+ *
+ * The APF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The APF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ * -->
+ */
 
-   import('modules::comments::biz','ArticleComment');
+import('modules::comments::biz', 'ArticleComment');
+
+/**
+ * @package modules::comments::date
+ * @class commentMapper
+ *
+ * Represents the data layer component of the comment function. Loads and saves entries.
+ *
+ * @author Christian W. Schäfer
+ * @version
+ * Version 0.1, 22.08.2007
+ */
+class commentMapper extends APFObject {
 
    /**
-    * @package modules::comments::date
-    * @class commentMapper
+    * @public
     *
-    * Represents the data layer component of the comment function. Loads and saves entries.
+    * Loads an article comment object by id. Can be used by the pager.
     *
-    * @author Christian W. Sch�fer
+    * @param string $commentId ID des Eintrags
+    * @return ArticleComment A comment object.
+    *
+    * @author Christian W.Sch�fer
     * @version
-    * Version 0.1, 22.08.2007
+    * Version 0.1, 22.08.2007<br />
     */
-   class commentMapper extends APFObject {
+   public function loadArticleCommentByID($commentId) {
 
-      /**
-       * @public
-       *
-       * Loads an article comment object by id. Can be used by the pager.
-       *
-       * @param string $commentId ID des Eintrags
-       * @return ArticleComment A comment object.
-       *
-       * @author Christian W.Sch�fer
-       * @version
-       * Version 0.1, 22.08.2007<br />
-       */
-      public function loadArticleCommentByID($commentId){
-
-         $SQL = &$this->getConnection();
-         $select = 'SELECT ArticleCommentID, Name, EMail, Comment, Date, Time 
+      $SQL = &$this->getConnection();
+      $select = 'SELECT ArticleCommentID, Name, EMail, Comment, Date, Time
                     FROM article_comments
-                    WHERE ArticleCommentID = \''.$commentId.'\';';
-         $result = $SQL->executeTextStatement($select);
-         return $this->__mapArticleComment2DomainObject($SQL->fetchData($result));
-
-       // end function
-      }
-
-      /**
-       * @public
-       *
-       * Saves a new comment.
-       *
-       * @param ArticleComment $comment The domain object to save.
-       *
-       * @author Christian Achatz
-       * @version
-       * Version 0.1, 22.08.2007<br />
-       */
-      public function saveArticleComment($comment){
-
-         $SQL = &$this->getConnection();
-         if($comment->get('ID') == null){
-            $insert = 'INSERT INTO article_comments (Name, EMail, Comment, Date, Time, CategoryKey) VALUES (\''.$comment->get('Name').'\',\''.$comment->get('EMail').'\',\''.$comment->get('Comment').'\',CURDATE(),CURTIME(),\''.$comment->get('CategoryKey').'\');';
-            $SQL->executeTextStatement($insert);
-          // end if
-         }
-
-       // end function
-      }
-
-      /**
-       * @public
-       *
-       * Returns the initialized database connection (reference!) for the
-       * current application instance.
-       *
-       * @return AbstractDatabaseHandler The database connection.
-       *
-       * @author Christian Achatz
-       * @version
-       * Version 0.1, 09.06.2008<br />
-       */
-      private function &getConnection(){
-
-         $cM = &$this->__getServiceObject('core::database','ConnectionManager');
-         $config = $this->getConfiguration('modules::comments','comments.ini');
-         $connectionKey = $config->getSection('Default')->getValue('Database.ConnectionKey');
-         if($connectionKey == null){
-            throw new InvalidArgumentException('[commentMapper::__getConnection()] The module\'s '
-               .'configuration file does not contain a valid database connection key. Please '
-               .'specify the database configuration according to the example configuration files!',
-               E_USER_ERROR);
-         }
-         return $cM->getConnection($connectionKey);
-
-       // end function
-      }
-
-      /**
-       * @private
-       *
-       * Mapps a database result set into s domain object.
-       *
-       * @param string[] $resultSet MySQL (database) result array.
-       * @return ArticleComment A initialized domain object.
-       *
-       * @author Christian W.Sch�fer
-       * @version
-       * Version 0.1, 22.08.2007<br />
-       */
-      private function __mapArticleComment2DomainObject($resultSet){
-
-         $comment = new ArticleComment();
-
-         if(isset($resultSet['ArticleCommentID'])){
-            $comment->set('ID',$resultSet['ArticleCommentID']);
-          // end if
-         }
-         if(isset($resultSet['Name'])){
-            $comment->set('Name',$resultSet['Name']);
-          // end if
-         }
-         if(isset($resultSet['EMail'])){
-            $comment->set('EMail',$resultSet['EMail']);
-          // end if
-         }
-         if(isset($resultSet['Comment'])){
-            $comment->set('Comment',$resultSet['Comment']);
-          // end if
-         }
-         if(isset($resultSet['Date'])){
-            $comment->set('Date',$resultSet['Date']);
-          // end if
-         }
-         if(isset($resultSet['Time'])){
-            $comment->set('Time',$resultSet['Time']);
-          // end if
-         }
-
-         return $comment;
-
-       // end function
-      }
-
-    // end class
+                    WHERE ArticleCommentID = \'' . $commentId . '\';';
+      $result = $SQL->executeTextStatement($select);
+      return $this->__mapArticleComment2DomainObject($SQL->fetchData($result));
    }
+
+   /**
+    * @public
+    *
+    * Saves a new comment.
+    *
+    * @param ArticleComment $comment The domain object to save.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 22.08.2007<br />
+    */
+   public function saveArticleComment($comment) {
+
+      $SQL = &$this->getConnection();
+      if ($comment->get('ID') == null) {
+         $insert = 'INSERT INTO article_comments
+                       (Name, EMail, Comment, Date, Time, CategoryKey)
+                       VALUES
+                       (\'' . $SQL->escapeValue($comment->get('Name')) . '\',\'' . $SQL->escapeValue($comment->get('EMail')) . '\',\'' . $SQL->escapeValue($comment->get('Comment')) . '\',CURDATE(),CURTIME(),\'' . $comment->get('CategoryKey') . '\');';
+         $SQL->executeTextStatement($insert);
+      }
+   }
+
+   /**
+    * @public
+    *
+    * Returns the initialized database connection (reference!) for the
+    * current application instance.
+    *
+    * @return AbstractDatabaseHandler The database connection.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 09.06.2008<br />
+    */
+   private function &getConnection() {
+
+      $cM = &$this->__getServiceObject('core::database', 'ConnectionManager');
+      $config = $this->getConfiguration('modules::comments', 'comments.ini');
+      $connectionKey = $config->getSection('Default')->getValue('Database.ConnectionKey');
+      if ($connectionKey == null) {
+         throw new InvalidArgumentException('[commentMapper::__getConnection()] The module\'s '
+                 . 'configuration file does not contain a valid database connection key. Please '
+                 . 'specify the database configuration according to the example configuration files!',
+                 E_USER_ERROR);
+      }
+      return $cM->getConnection($connectionKey);
+   }
+
+   /**
+    * @private
+    *
+    * Mapps a database result set into s domain object.
+    *
+    * @param string[] $resultSet MySQL (database) result array.
+    * @return ArticleComment A initialized domain object.
+    *
+    * @author Christian W.Sch�fer
+    * @version
+    * Version 0.1, 22.08.2007<br />
+    */
+   private function __mapArticleComment2DomainObject($resultSet) {
+      $comment = new ArticleComment();
+      $comment->set('ID', $resultSet['ArticleCommentID']);
+      $comment->set('Name', $resultSet['Name']);
+      $comment->set('EMail', $resultSet['EMail']);
+      $comment->set('Comment', $resultSet['Comment']);
+      $comment->set('Date', $resultSet['Date']);
+      $comment->set('Time', $resultSet['Time']);
+      return $comment;
+   }
+
+}
 ?>
