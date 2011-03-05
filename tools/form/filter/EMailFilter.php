0fc934d1c911ee5a7053c8e49d66ea82ec9af468
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-03-05 18:32:31 +0000

    Adapted email filter regexp to be less sensitive. See http://forum.adventure-php-framework.org/de/viewtopic.php?f=8&t=450&p=5810#p5810 for more details.

--- tools/form/filter/EMailFilter.php
+++ tools/form/filter/EMailFilter.php
@@ -1,44 +1,41 @@
 <?php
-   /**
-    * <!--
-    * This file is part of the adventure php framework (APF) published under
-    * http://adventure-php-framework.org.
-    *
-    * The APF is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU Lesser General Public License as published
-    * by the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
-    *
-    * The APF is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    * GNU Lesser General Public License for more details.
-    *
-    * You should have received a copy of the GNU Lesser General Public License
-    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-    * -->
-    */
-    
-   import('tools::form::filter','AbstractFormFilter');
-   
-   /**
-    * @package tools::form::filter
-    * @class EMailFilter
-    *
-    * Implements a filter, that removes all characters, that are not
-    * allowed in email addresses.
-    *
-    * @author Christian Achatz
-    * @version
-    * Version 0.1, 11.09.2009<br />
-    */
-   class EMailFilter extends AbstractFormFilter {
+/**
+ * <!--
+ * This file is part of the adventure php framework (APF) published under
+ * http://adventure-php-framework.org.
+ *
+ * The APF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The APF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ * -->
+ */
+import('tools::form::filter', 'AbstractFormFilter');
 
-      public function filter($input){
-         return preg_replace('/[^A-Za-z0-9@\-\.]/','',$input);
-       // end function
-      }
+/**
+ * @package tools::form::filter
+ * @class EMailFilter
+ *
+ * Implements a filter, that removes all characters, that are not
+ * allowed in email addresses.
+ *
+ * @author Christian Achatz
+ * @version
+ * Version 0.1, 11.09.2009<br />
+ */
+class EMailFilter extends AbstractFormFilter {
 
-    // end class
+   public function filter($input) {
+      return preg_replace('/[^A-Za-z0-9@\-\._]/', '', $input);
    }
+
+}
 ?>
