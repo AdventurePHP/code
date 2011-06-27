68da97f2824aee82f6e22735fa57916e924a67da
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-06-27 20:20:48 +0000

    Removed unused configuration container variable.

--- extensions/jscsspackager/biz/actions/JsCssPackagerAction.php
+++ extensions/jscsspackager/biz/actions/JsCssPackagerAction.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * <!--
  * This file is part of the adventure php framework (APF) published under
@@ -18,93 +19,82 @@
  * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
  * -->
  */
-
-import('tools::http','HeaderManager');
+import('tools::http', 'HeaderManager');
 
 /**
- *  @namespace extensions::jscsspackager::biz::actions
- *  @class JsCssPackagerAction
+ * @namespace extensions::jscsspackager::biz::actions
+ * @class JsCssPackagerAction
  *
  *  Implements an FC-action which returns multiple .css and .js files in one package
  *
- *  @example
+ * @example
  *  Javascript:
  *  /APF/sites/test/index.php?extensions_jscsspackager_biz-action:jcp=package:form_clientvalidators_all.js
  *  Css:
  *  /APF/sites/test/index.php?extensions_jscsspackager_biz-action:jcp=package:form_clientvaltest.css
  *
- *  @author Ralf Schubert <ralf.schubert@the-screeze.de>
- *  @version 1.0, 18.03.2010<br>
+ * @author Ralf Schubert <ralf.schubert@the-screeze.de>
+ * @version 1.0, 18.03.2010<br>
  */
 final class JsCssPackagerAction extends AbstractFrontcontrollerAction {
 
-    /**
-     * @var Configuration Contains the configuration for packages
-     */
-    protected $__Cfg = null;
+   public function run() {
+      $package = $this->getInput()->getAttribute('package');
 
-    public function JsCssPackagerAction() {
-    }
-
-    public function run() {
-        $package = $this->getInput()->getAttribute('package');
-
-        // Check if all required attributes are given
-        if(empty($package)) {
-            throw new InvalidArgumentException('[JSPackagerAction::run()] The attribute
+      // Check if all required attributes are given
+      if (empty($package)) {
+         throw new InvalidArgumentException('[JSPackagerAction::run()] The attribute
                 "package" is empty or not present.');
-            exit();
-        }
+      }
 
-        $packageExpl = explode('.', $package);
-        if(count($packageExpl) !== 2) {
+      $packageExpl = explode('.', $package);
+      if (count($packageExpl) !== 2) {
+         throw new InvalidArgumentException('[JSPackagerAction::run()] The attribute
+                 "package" has to be like "packagename.type", with type
+                 beeing "js" or "css".');
+      }
+
+      $packName = $packageExpl[0];
+      $packType = $packageExpl[1];
+      // check if correct type is given. If not exit() for security reasons.
+      switch ($packType) {
+         case 'css':
+            $mimeType = 'text/css';
+            break;
+         case 'js':
+            $mimeType = 'text/javascript';
+            break;
+         default:
             throw new InvalidArgumentException('[JSPackagerAction::run()] The attribute
                  "package" has to be like "packagename.type", with type
                  beeing "js" or "css".');
-            exit();
-        }
+      }
 
-        $packName = $packageExpl[0];
-        $packType = $packageExpl[1];
-        // check if correct type is given. If not exit() for security reasons.
-        switch($packType) {
-            case 'css':
-                $mimeType = 'text/css';
-                break;
-            case 'js':
-                $mimeType = 'text/javascript';
-                break;
-            default:
-                throw new InvalidArgumentException('[JSPackagerAction::run()] The attribute
-                 "package" has to be like "packagename.type", with type
-                 beeing "js" or "css".');
-                exit();
-        }
-        
-        // Check if gzip is supported
-        $acceptGzip = false;
-        if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')){
-            $acceptGzip = true;
-            // send gzip header
-            HeaderManager::send("Content-Encoding: gzip");
-        }
-        
-        /* @var $packager JsCssPackager */
-        $packager = $this->getAndInitServiceObject('extensions::jscsspackager::biz', 'JsCssPackager', null);
-        $output = $packager->getPackage($packName, $acceptGzip);
-        // Get ClientCachePeriod (in days), and convert to seconds
-        $clientCachePeriod = $packager->getClientCachePeriod($packName)*86400;
+      // Check if gzip is supported
+      $acceptGzip = false;
+      if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+         $acceptGzip = true;
+         // send gzip header
+         HeaderManager::send("Content-Encoding: gzip");
+      }
 
-        // send headers to allow caching
-        HeaderManager::send('Cache-Control: public; max-age='.$clientCachePeriod, true);
-        $modifiedDate = date('D, d M Y H:i:s \G\M\T', time());
-        HeaderManager::send('Last-Modified: '.$modifiedDate, true);
-        $expiresDate = date('D, d M Y H:i:s \G\M\T', time() + $clientCachePeriod);
-        HeaderManager::send('Expires: '.$expiresDate, true);
-        HeaderManager::send('Content-type: '.$mimeType);
-        
-        echo $output;
-        exit();
-    }
+      /* @var $packager JsCssPackager */
+      $packager = $this->getAndInitServiceObject('extensions::jscsspackager::biz', 'JsCssPackager', null);
+      $output = $packager->getPackage($packName, $acceptGzip);
+      // Get ClientCachePeriod (in days), and convert to seconds
+      $clientCachePeriod = $packager->getClientCachePeriod($packName) * 86400;
+
+      // send headers to allow caching
+      HeaderManager::send('Cache-Control: public; max-age=' . $clientCachePeriod, true);
+      $modifiedDate = date('D, d M Y H:i:s \G\M\T', time());
+      HeaderManager::send('Last-Modified: ' . $modifiedDate, true);
+      $expiresDate = date('D, d M Y H:i:s \G\M\T', time() + $clientCachePeriod);
+      HeaderManager::send('Expires: ' . $expiresDate, true);
+      HeaderManager::send('Content-type: ' . $mimeType);
+
+      echo $output;
+      exit();
+   }
 }
+
 ?>
