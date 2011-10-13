c81191134ade7b511c1a5b80eb0a327883f0242e
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-10-13 16:23:30 +0000

    Reformatted code.

--- tools/form/validator/TextLengthValidator.php
+++ tools/form/validator/TextLengthValidator.php
@@ -1,143 +1,140 @@
 <?php
+/**
+ * <!--
+ * This file is part of the adventure php framework (APF) published under
+ * http://adventure-php-framework.org.
+ *
+ * The APF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The APF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ * -->
+ */
+import('tools::form::validator', 'TextFieldValidator');
+
+/**
+ * @package tools::form::validator
+ * @class TextLengthValidator
+ *
+ * Validates a given form control to contain a string with a defined length.
+ * The default value is three, to configure the length, please specify the
+ * <em>minlength</em> and <em>maxlength</em> attribute within the target form
+ * control definition.
+ *
+ * @author Christian Achatz
+ * @version
+ * Version 0.1, 29.08.2009<br />
+ * Version 0.2, 04.07.2010 (Added mode "strict")<br />
+ */
+class TextLengthValidator extends TextFieldValidator {
+
+   private static $MIN_LENGTH_ATTRIBUTE_NAME = 'minlength';
+   private static $MAX_LENGTH_ATTRIBUTE_NAME = 'maxlength';
+   private static $MODE_ATTRIBUTE_NAME = 'mode';
+
    /**
-    * <!--
-    * This file is part of the adventure php framework (APF) published under
-    * http://adventure-php-framework.org.
+    * @public
     *
-    * The APF is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU Lesser General Public License as published
-    * by the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
+    * Re-implements the <em>validate()</em> method for the text length validator.
+    * Supports min and max length definition of the text to validate.
     *
-    * The APF is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    * GNU Lesser General Public License for more details.
+    * @param string $input The text to validate.
+    * @return boolean true, in case the control to validate is considered valid, false otherwise.
     *
-    * You should have received a copy of the GNU Lesser General Public License
-    * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
-    * -->
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 06.02.2010<br />
     */
+   public function validate($input) {
 
-   import('tools::form::validator','TextFieldValidator');
+      $minlength = $this->getMinLength();
+      $maxlength = $this->getMaxLength();
+      $mode = $this->getMode();
+
+      // mode strict needs trim() on $input
+      if ($mode === 'strict') {
+         $input = trim($input);
+      }
+
+      // the maxlength beeing null, the text may contain an infinite number of characters
+      if ($maxlength === 0) {
+         if (!empty($input) && strlen($input) >= $minlength) {
+            return true;
+         }
+      }
+      else {
+         if (!empty($input) && strlen($input) >= $minlength && strlen($input) <= $maxlength) {
+            return true;
+         }
+      }
+      return false;
+   }
 
    /**
-    * @package tools::form::validator
-    * @class TextLengthValidator
+    * @private
     *
-    * Validates a given form control to contain a string with a defined length.
-    * The default value is three, to configure the length, please specify the
-    * <em>minlength</em> and <em>maxlength</em> attribute within the target form
-    * control definition.
+    * Returns the min length of the text, that must be contained within
+    * the target control. Tries to load the min length from the
+    * <em>minlength</em> attribute within the target form control definition.
+    *
+    * @return int The min length of the text.
     *
     * @author Christian Achatz
     * @version
     * Version 0.1, 29.08.2009<br />
-    * Version 0.2, 04.07.2010 (Added mode "strict")<br />
     */
-   class TextLengthValidator extends TextFieldValidator {
-
-      private static $MIN_LENGTH_ATTRIBUTE_NAME = 'minlength';
-      private static $MAX_LENGTH_ATTRIBUTE_NAME = 'maxlength';
-      private static $MODE_ATTRIBUTE_NAME = 'mode';
-
-      /**
-       * @public
-       *
-       * Re-implements the <em>validate()</em> method for the text length validator.
-       * Supports min and max length definition of the text to validate.
-       *
-       * @param string The text to validate.
-       * @return boolean true, in case the control to validate is considered valid, false otherwise.
-       *
-       * @author Christian Achatz
-       * @version
-       * Version 0.1, 06.02.2010<br />
-       */
-      public function validate($input){
-         
-         $minlength = $this->getMinLength();
-         $maxlength = $this->getMaxLength();
-         $mode = $this->getMode();
-         
-         // mode strict needs trim() on $input
-         if($mode === 'strict'){
-            $input = trim($input);
-         }
-
-         // the maxlength beeing null, the text may contain an infinite number of characters
-         if($maxlength === 0){
-            if(!empty($input) && strlen($input) >= $minlength){
-               return true;
-            }
-         }
-         else {
-            if(!empty($input) && strlen($input) >= $minlength && strlen($input) <= $maxlength){
-               return true;
-            }
-         }
-         return false;
-         
-       // end function
+   private function getMinLength() {
+      $minLength = $this->__Control->getAttribute(self::$MIN_LENGTH_ATTRIBUTE_NAME);
+      if ($minLength === null) {
+         $minLength = 3;
       }
-
-      /**
-       * @private
-       *
-       * Returns the min length of the text, that must be contained within
-       * the target control. Tries to load the min length from the
-       * <em>minlength</em> attribute within the target form control definition.
-       *
-       * @return int The min length of the text.
-       *
-       * @author Christian Achatz
-       * @version
-       * Version 0.1, 29.08.2009<br />
-       */
-      private function getMinLength(){
-         $minLength = $this->__Control->getAttribute(self::$MIN_LENGTH_ATTRIBUTE_NAME);
-         if($minLength === null){
-            $minLength = 3;
-         }
-         return (int)$minLength;
-      }
-
-      /**
-       * @private
-       *
-       * Returns the max length of the text, that must be contained within
-       * the target control. Tries to load the max length from the
-       * <em>maxlength</em> attribute within the target form control definition.
-       *
-       * @return int The max length of the text.
-       *
-       * @author Christian Achatz
-       * @version
-       * Version 0.1, 06.02.2010<br />
-       */
-      private function getMaxLength(){
-         // max length beeing null is ok, because we consider it to indicate infinite length!
-         $maxLength = $this->__Control->getAttribute(self::$MAX_LENGTH_ATTRIBUTE_NAME);
-         return (int)$maxLength;
-      }
-      
-      /**
-       * @private
-       *
-       * Returns the mode of validating, which should be used on the target control. 
-       * Tries to load the mode from the <em>mode</em> attribute within the target form control definition.
-       *
-       * @return string The mode name.
-       *
-       * @author Ralf Schubert
-       * @version
-       * Version 0.1, 04.07.2010<br />
-       */
-      private function getMode(){
-        $mode = $this->__Control->getAttribute(self::$MODE_ATTRIBUTE_NAME);
-        return $mode;
-      }
-      
-    // end function
+      return (int)$minLength;
    }
+
+   /**
+    * @private
+    *
+    * Returns the max length of the text, that must be contained within
+    * the target control. Tries to load the max length from the
+    * <em>maxlength</em> attribute within the target form control definition.
+    *
+    * @return int The max length of the text.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 06.02.2010<br />
+    */
+   private function getMaxLength() {
+      // max length being null is ok, because we consider it to indicate infinite length!
+      $maxLength = $this->__Control->getAttribute(self::$MAX_LENGTH_ATTRIBUTE_NAME);
+      return (int)$maxLength;
+   }
+
+   /**
+    * @private
+    *
+    * Returns the mode of validating, which should be used on the target control.
+    * Tries to load the mode from the <em>mode</em> attribute within the target form control definition.
+    *
+    * @return string The mode name.
+    *
+    * @author Ralf Schubert
+    * @version
+    * Version 0.1, 04.07.2010<br />
+    */
+   private function getMode() {
+      $mode = $this->__Control->getAttribute(self::$MODE_ATTRIBUTE_NAME);
+      return $mode;
+   }
+
+}
+
 ?>
