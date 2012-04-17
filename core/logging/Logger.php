05668b6633db6715eae648c04375dba3137c3bda
Christian Achatz <christian.achatz@adventure-php-framework.org>
2012-04-17 20:31:12 +0000

    Enhanced the default logger: * Enable different log entry types (in future versions only!) * Enable threshold definition for trace statements * Prepared refactoring for later versions (combination with AdvancedLogger)

--- core/logging/Logger.php
+++ core/logging/Logger.php
@@ -50,65 +50,38 @@ class LoggerException extends Exception {
  * @package core::logging
  * @class LogEntry
  *
- * Implements a log entry.
+ * Defines the scheme of a log entry.
  *
  * @author Christian Achatz
  * @version
- * Version 0.1, 29.03.2007<br />
+ * Version 0.1, 17.04.2012<br />
  */
-class LogEntry {
+interface LogEntry {
 
    /**
     * The following constants define common severity types to be used within log statements.
     */
    const SEVERITY_TRACE = 'TRACE';
+   const SEVERITY_DEBUG = 'DEBUG';
    const SEVERITY_INFO = 'INFO';
    const SEVERITY_WARNING = 'WARN';
    const SEVERITY_ERROR = 'ERROR';
    const SEVERITY_FATAL = 'FATAL';
 
    /**
-    * @private
-    * @var string Date of the message.
-    */
-   private $date;
-
-   /**
-    * @private
-    * @var string Time of the message.
-    */
-   private $time;
-
-   /**
-    * @private
-    * @var string Message text.
-    */
-   private $message;
-
-   /**
-    * @private
-    * @var string Message type.
-    */
-   private $severity;
-
-   /**
     * @public
     *
     * Creates a new log entry.
     *
+    * @param string $target The log target of this entry.
     * @param string $message Desired error message.
-    * @param string $type Error message type.
+    * @param string $severity Error message severity.
     *
     * @author Christian Achatz
     * @version
     * Version 0.1, 29.03.2007<br />
     */
-   public function __construct($message, $severity) {
-      $this->date = date('Y-m-d');
-      $this->time = date('H:i:s');
-      $this->message = $message;
-      $this->severity = $severity;
-   }
+   public function __construct($target, $message, $severity);
 
    /**
     * @public
@@ -121,8 +94,92 @@ class LogEntry {
     * @version
     * Version 0.1, 29.03.2007<br />
     */
-   public function toString() {
+   public function __toString();
+
+   /**
+    * @public
+    *
+    * Returns the target identifier of this log entry. Merely, - but depending
+    * on the LogWriter implementation - this is the name of the log file or at
+    * least the main part of it.
+    *
+    * @return string The log target key.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 17.04.2012<br />
+    */
+   public function getLogTarget();
+
+   /**
+    * @public
+    *
+    * Returns the severity of this log entry.
+    *
+    * @return string The log entry's severity.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 17.04.2012<br />
+    */
+   public function getSeverity();
+}
+
+/**
+ * @package core::logging
+ * @class LogEntry
+ *
+ * Implements a log entry.
+ *
+ * @author Christian Achatz
+ * @version
+ * Version 0.1, 29.03.2007<br />
+ */
+class SimpleLogEntry implements LogEntry {
+
+   /**
+    * @var string Date of the message.
+    */
+   protected $date;
+
+   /**
+    * @var string Time of the message.
+    */
+   protected $time;
+
+   /**
+    * @var string Message text.
+    */
+   protected $message;
+
+   /**
+    * @var string Message type.
+    */
+   protected $severity;
+
+   /**
+    * @var string The log target (here: the body of the log file name)
+    */
+   protected $target;
+
+   public function __construct($target, $message, $severity) {
+      $this->date = date('Y-m-d');
+      $this->time = date('H:i:s');
+      $this->target = $target;
+      $this->message = $message;
+      $this->severity = $severity;
+   }
+
+   public function __toString() {
       return '[' . $this->date . ' ' . $this->time . '] [' . $this->severity . '] ' . $this->message;
+   }
+
+   public function getLogTarget() {
+      return $this->target;
+   }
+
+   public function getSeverity() {
+      return $this->severity;
    }
 
 }
@@ -141,20 +198,49 @@ class LogEntry {
  */
 class Logger {
 
+   public static $LOGGER_THRESHOLD_ALL = array(
+      LogEntry::SEVERITY_TRACE,
+      LogEntry::SEVERITY_DEBUG,
+      LogEntry::SEVERITY_WARNING,
+      LogEntry::SEVERITY_INFO,
+      LogEntry::SEVERITY_ERROR,
+      LogEntry::SEVERITY_FATAL
+   );
+
+   public static $LOGGER_THRESHOLD_WARN = array(
+      LogEntry::SEVERITY_WARNING,
+      LogEntry::SEVERITY_INFO,
+      LogEntry::SEVERITY_ERROR,
+      LogEntry::SEVERITY_FATAL
+   );
+
+   public static $LOGGER_THRESHOLD_INFO = array(
+      LogEntry::SEVERITY_INFO,
+      LogEntry::SEVERITY_ERROR,
+      LogEntry::SEVERITY_FATAL
+   );
+
+   public static $LOGGER_THRESHOLD_ERROR = array(
+      LogEntry::SEVERITY_ERROR,
+      LogEntry::SEVERITY_FATAL
+   );
+
    /**
-    * @protected
+    * @var array Defines the severity types that are witten to the log file.
+    */
+   protected $logThreshold;
+
+   /**
     * @var LogEntry[] Log entry store.
     */
    protected $logEntries = array();
 
    /**
-    * @private
     * @var string Directory, where log files are stored.
     */
    protected $logDir;
 
    /**
-    * @private
     * @var string Permission that is applied to a newly created log folder.
     */
    protected $logFolderPermissions = 0777;
@@ -170,7 +256,6 @@ class Logger {
    protected $logEntryCount = 0;
 
    /**
-    * @private
     * @var string Newline sign. Uses the PHP's standard newline sign if not configured in different way.
     */
    protected static $CRLF = PHP_EOL;
@@ -189,6 +274,7 @@ class Logger {
     */
    public function __construct() {
       $this->logDir = Registry::retrieve('apf::core', 'LogDir');
+      $this->logThreshold = self::$LOGGER_THRESHOLD_WARN;
    }
 
    /**
@@ -210,6 +296,22 @@ class Logger {
    /**
     * @public
     *
+    * Let's you set the logging threshold. This is a list of severities that are
+    * written to the log. All other severities are ignored.
+    *
+    * @param array $threshold The threshold configuration to apply to the log statements.
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 18.03.2012<br />
+    */
+   public function setLogThreshold(array $threshold) {
+      $this->logThreshold = $threshold;
+   }
+
+   /**
+    * @public
+    *
     * Create a log entry.
     *
     * @param string $logFileName Name of the log file to log to
@@ -221,13 +323,35 @@ class Logger {
     * Version 0.1, 29.03.2007<br />
     * Version 0.2, 02.05.2011 (Flushes the log buffer implicitly after a configured number of entries)<br />
     */
-   public function logEntry($logFileName, $message, $type = 'INFO') {
+   public function logEntry($logFileName, $message, $type = LogEntry::SEVERITY_INFO) {
+      $this->addEntry(new SimpleLogEntry($logFileName, $message, $type));
+   }
 
-      $this->logEntries[$logFileName][] = new LogEntry($message, $type);
+   /**
+    * @protected
+    *
+    * Internal method to create a log entry.
+    * <p/>
+    * Will be opened to the outside world in future versions in conjunction
+    * with the refactoring of the Logger and the combination with the
+    * AdvancedLogger.
+    *
+    * @param LogEntry $entry The log entry to add
+    *
+    * @author Christian Achatz
+    * @version
+    * Version 0.1, 17.04.2012<br />
+    */
+   protected function addEntry(LogEntry $entry) {
 
-      // flush the log buffer in case the maximum number of entries is reached.
-      $this->logEntryCount++;
-      
+      // check for severity to match the threshold definition
+      if (in_array($entry->getSeverity(), $this->logThreshold)) {
+         $this->logEntries[$entry->getLogTarget()][] = $entry;
+
+         // flush the log buffer in case the maximum number of entries is reached.
+         $this->logEntryCount++;
+      }
+
       if ($this->logEntryCount > $this->maxBufferLength) {
          $this->flushLogBuffer();
       }
@@ -255,21 +379,21 @@ class Logger {
 
             // try to create non existing log dir
             if (!@mkdir($this->logDir, $this->logFolderPermissions)) {
-               throw new LoggerException('[Logger->Logger()] The log directory "'
-                       . $this->logDir . '" cannot be created du to permission restrictions! '
-                       . 'Please check config and specify the "LogDir" (namespace: "apf::core") '
-                       . 'parameter in the registry!');
+               throw new LoggerException('[Logger->flushLogBuffer()] The log directory "'
+                     . $this->logDir . '" cannot be created du to permission restrictions! '
+                     . 'Please check config and specify the "LogDir" (namespace: "apf::core") '
+                     . 'parameter in the registry!');
             }
 
          }
 
-         // flush entries to the filesystem
+         // flush entries to the files
          foreach ($this->logEntries as $logFileName => $logEntries) {
 
             // generate complete log file name
             $logFileName = $this->getLogFileName($logFileName);
 
-            // generate complete log file pathe
+            // generate complete log file path
             $logFile = $this->logDir . '/' . $logFileName;
 
             if (count($logEntries) > 0) {
@@ -277,10 +401,10 @@ class Logger {
                $lFH = fopen($logFile, 'a+');
 
                for ($i = 0; $i < count($logEntries); $i++) {
-                  fwrite($lFH, $logEntries[$i]->toString() . self::$CRLF);
+                  fwrite($lFH, $logEntries[$i]->__toString() . self::$CRLF);
                }
 
-               // close file!
+               // close file to avoid deadlocks!
                fclose($lFH);
             }
          }
@@ -292,16 +416,16 @@ class Logger {
    }
 
    /**
-    *  @protected
+    * @protected
     *
     *  Returns the name of the log file by the body of the name. Each log file will be named
     * like jjjj_mm_dd__{filename}.log.
     *
-    *  @param string $fileName Name of the log file
-    *  @return string Complete file name, that contains a date prefix and an file extension
+    * @param string $fileName Name of the log file
+    * @return string Complete file name, that contains a date prefix and an file extension
     *
-    *  @author Christian Achatz
-    *  @version
+    * @author Christian Achatz
+    * @version
     *  Version 0.1, 29.03.2007<br />
     */
    protected function getLogFileName($fileName) {
@@ -309,4 +433,3 @@ class Logger {
    }
 
 }
-?>
