2212c661a3a1c4a82622c709a3a2b35b45d1c6d4
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-06 20:07:07 +0000

    Added migration for input class definition.

--- migration/migrate_fc_configuration.php
+++ migration/migrate_fc_configuration.php
@@ -5,11 +5,19 @@ $files = find('config', '*actionconfig.ini');
 
 $search = '#FC.ActionNamespace ?= ?"([A-Za-z0-9:\-]+)"([\n|\r\n]+)?FC.ActionClass ?= ?"([A-Za-z0-9\-]+)"#';
 
+$searchInput = '#FC.InputNamespace ?= ?"([A-Za-z0-9:\-]+)"([\n|\r\n]+)?FC.InputClass ?= ?"([A-Za-z0-9\-]+)"#';
+
 foreach ($files as $file) {
    $content = file_get_contents($file);
 
+   // migrate action implementation
    $content = preg_replace_callback($search, function ($matches) {
       return 'FC.ActionClass = "APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[3] . '"';
+   }, $content);
+
+   // migrate input implementation
+   $content = preg_replace_callback($searchInput, function ($matches) {
+      return 'FC.InputClass = "APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[3] . '"';
    }, $content);
 
    file_put_contents($file, $content);
