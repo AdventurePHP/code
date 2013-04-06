eb01a6eea9a2b835a3129cf15ac2cdbc01ab5026
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-06 16:19:20 +0000

    Added document controller migration script.

---
+++ migration/migrate_document_controller_statements.php
@@ -0,0 +1,16 @@
+<?php
+include(dirname(__FILE__) . '/migrate_base.php');
+
+$files = filterApfDirectories(find('.', '*.html'));
+
+$search = '#<@controller([ |\n|\r\n]+)namespace ?= ?"([A-Za-z0-9:\-]+)"([\*| |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"([ |\n|\r\n]+)@>#';
+
+foreach ($files as $file) {
+   $content = file_get_contents($file);
+
+   $content = preg_replace_callback($search, function ($matches) {
+      return '<@controller' . $matches[1] . 'class="APF\\' . str_replace('::', '\\', $matches[2]) . '\\' . $matches[4] . '"' . $matches[5] . '@>';
+   }, $content);
+
+   file_put_contents($file, $content);
+}
