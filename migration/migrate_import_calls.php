ee6ee3551ef16225b696d2ca5e5d57be367900fa
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-06 13:14:42 +0000

    Updated script to migrate import() calls.

--- migration/migrate_import_calls.php
+++ migration/migrate_import_calls.php
@@ -3,16 +3,14 @@ include(dirname(__FILE__) . '/migrate_base.php');
 
 $files = find('.', '*.php');
 
-$search = '#import(\'([A-Za-z0-9:\-]+)\', ?\'([A-Za-z0-9_]+)\');#m';
+$search = '#import\(\'([A-Za-z0-9:\-]+)\', ?\'([A-Za-z0-9_]+)\'\);#m';
 
 foreach ($files as $file) {
    $content = file_get_contents($file);
 
-   // replace single calls
    $content = preg_replace_callback($search, function ($matches) {
-      $new = 'use APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[2] . ';';
-      echo $matches[0] . ' --> ' . $new;
+      return 'use APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[2] . ';';
    }, $content);
 
-   //file_put_contents($file, $content);
+   file_put_contents($file, $content);
 }
