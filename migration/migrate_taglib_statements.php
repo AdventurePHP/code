96c14c6191e170c4630975bac24f2c450bc72f8e
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-08 20:50:25 +0000

    Added migration script for form validators + migrated form validators.

--- migration/migrate_taglib_statements.php
+++ migration/migrate_taglib_statements.php
@@ -14,6 +14,12 @@ $searchAppendNodeNamespace = '#<([A-Za-z0-9\-]+):appendnode([\*| |\n|\r\n]+)name
 $searchHeaderJs = '#<htmlheader:addjs([\*| |\n|\r\n]+)namespace="([A-Za-z0-9:\-]+)"#';
 $searchHeaderCss = '#<htmlheader:addcss([\*| |\n|\r\n]+)namespace="([A-Za-z0-9:\-]+)"#';
 
+// respect explicit and none-explicit calls (explicits first!)
+$searchAddValidatorExplicit = '#<([A-Za-z0-9\-]+):addvalidator([ |\n|\r\n]+)namespace ?= ?"([A-Za-z0-9:\-]+)"([\*| |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
+$searchAddValidator = '#<([A-Za-z0-9\-]+):addvalidator([ |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
+$searchAddFilterExplicit = '#<([A-Za-z0-9\-]+):addfilter([ |\n|\r\n]+)namespace ?= ?"([A-Za-z0-9:\-]+)"([\*| |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
+$searchAddFilter = '#<([A-Za-z0-9\-]+):addfilter([ |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
+
 foreach ($files as $file) {
    $content = file_get_contents($file);
 
@@ -43,6 +49,26 @@ foreach ($files as $file) {
    }, $content);
    $content = preg_replace_callback($searchHeaderCss, function ($matches) {
       return '<htmlheader:addcss' . $matches[1] . 'namespace="APF\\' . str_replace('::', '\\', $matches[2]) . '"';
+   }, $content);
+
+   // replace explicit addvalidator
+   $content = preg_replace_callback($searchAddValidatorExplicit, function ($matches) {
+      return '<' . $matches[1] . ':addvalidator' . $matches[2] . 'class="APF\\' . str_replace('::', '\\', $matches[3]) . '\\' . $matches[5] . '"';
+   }, $content);
+
+   // replace none-explicit addvalidator
+   $content = preg_replace_callback($searchAddValidator, function ($matches) {
+      return '<' . $matches[1] . ':addvalidator' . $matches[2] . 'class="APF\tools\form\validator\\' . $matches[3] . '"';
+   }, $content);
+
+   // replace explicit addfilter
+   $content = preg_replace_callback($searchAddFilterExplicit, function ($matches) {
+      return '<' . $matches[1] . ':addfilter' . $matches[2] . 'class="APF\\' . str_replace('::', '\\', $matches[3]) . '\\' . $matches[5] . '"';
+   }, $content);
+
+   // replace none-explicit addfilter
+   $content = preg_replace_callback($searchAddFilter, function ($matches) {
+      return '<' . $matches[1] . ':addfilter' . $matches[2] . 'class="APF\tools\form\filter\\' . $matches[3] . '"';
    }, $content);
 
    file_put_contents($file, $content);
