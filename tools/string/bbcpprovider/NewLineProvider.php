fa70ae2b8117b2734ae23c1770b9c49cf521fa5f
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-08 17:02:29 +0000

    Added missing use statements.

--- tools/string/bbcpprovider/NewLineProvider.php
+++ tools/string/bbcpprovider/NewLineProvider.php
@@ -20,6 +20,7 @@ namespace APF\tools\string\bbcpprovider;
  * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
  * -->
  */
+use APF\tools\string\BBCodeParserProvider;
 
 /**
  * @package APF\tools\string\bbcpprovider
