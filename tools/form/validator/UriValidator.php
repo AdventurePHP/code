c79e73dae28af5c26e6f8f0278b4e950dffe6c74
Jan Wiese <jan.wiese@adventure-php-framework.org>
2013-05-26 17:02:23 +0000

    Added validator for URL to form-tools (checked in for Screeze)

---
+++ tools/form/validator/UriValidator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace APF\tools\form\validator;
+
+/**
+ * <!--
+ * This file is part of the adventure php framework (APF) published under
+ * http://adventure-php-framework.org.
+ *
+ * The APF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The APF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the APF. If not, see http://www.gnu.org/licenses/lgpl-3.0.txt.
+ * -->
+ */
+use APF\tools\form\validator\TextFieldValidator;
+
+/**
+ * @package APF\tools\form\validator
+ * @class UriValidator
+ *
+ * Validates a given form control to contain a syntactically correct uri.
+ *
+ * @author Ralf Schubert
+ * @version
+ * Version 0.1, 26.05.2013<br />
+ */
+class UriValidator extends TextFieldValidator {
+
+    public function validate($input) {
+        if (!empty($input) &&
+                preg_match('/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i', $input)) {
+            return true;
+        }
+        return false;
+    }
+
+}
