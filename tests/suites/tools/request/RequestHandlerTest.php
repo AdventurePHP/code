714e6cb2fb83f79c4bbe7db27cdccbbb113236f7
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-08-26 17:11:31 +0000

    ID#71: refactored RequestHandler to include functionality of PostHandler and also dealing with GET parameters. Documentation of RequestHandler still to be updated.

--- tests/suites/tools/request/RequestHandlerTest.php
+++ tests/suites/tools/request/RequestHandlerTest.php
@@ -10,22 +10,32 @@ use APF\tools\request\RequestHandler;
  * Tests the RequestHandler. Written due to bug with "0" values.
  *
  * @author Christian Achatz
+ * @version
+ * Version 0.1<br />
+ * Version 0.2, 26.08.2013 (Refactoring or RequestHandler to include functionality of PostHandler)<br />
  */
 class RequestHandlerTest extends \PHPUnit_Framework_TestCase {
 
+   const PARAMETER_FOO = 'foo';
+   const PARAMETER_BAR = 'bar';
+
    protected function setUp() {
+      $_GET = array();
+      $_POST = array();
       $_REQUEST = array();
    }
 
    protected function tearDown() {
       unset($_REQUEST);
+      unset($_GET);
+      unset($_POST);
    }
 
    public function testSimpleValues() {
 
       $testData = array(
-         'foo' => 'bar',
-         'bar' => '!"§$%&/()=}][{1223{[]}',
+         self::PARAMETER_FOO => self::PARAMETER_BAR,
+         self::PARAMETER_BAR => '!"§$%&/()=}][{1223{[]}',
          '4356J76tiojoigztfgu' => 'µ0815{[]}\][€edrftgzhujhgvcdewrtzujhgfdsaölkjhiug u qwhph phpihpiqw ph p9h pwoq',
          'urlencoded-data' => urlencode('This is a message with spaces and other content like "/", "&", "%", "$", and "§".'),
          0 => 'zero',
@@ -34,6 +44,19 @@ class RequestHandlerTest extends \PHPUnit_Framework_TestCase {
 
       $_REQUEST = array();
       foreach ($testData as $key => $value) {
+
+         $_GET[$key] = $value;
+         assertEquals(
+            RequestHandler::getValue($key, null, RequestHandler::USE_GET_PARAMS),
+            $value
+         );
+
+         $_POST[$key] = $value;
+         assertEquals(
+            RequestHandler::getValue($key, null, RequestHandler::USE_POST_PARAMS),
+            $value
+         );
+
          $_REQUEST[$key] = $value;
          assertEquals(
             RequestHandler::getValue($key),
@@ -45,28 +68,88 @@ class RequestHandlerTest extends \PHPUnit_Framework_TestCase {
 
    public function testEmptyValues() {
 
-      $_REQUEST = array();
-
+      $_GET = array();
       assertNull(
-         RequestHandler::getValue('foo')
+         RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_GET_PARAMS)
       );
 
-      $_REQUEST['bar'] = '';
-
+      $_POST = array();
       assertNull(
-         RequestHandler::getValue('bar')
+         RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_POST_PARAMS)
+      );
+
+      $_REQUEST = array();
+      assertNull(
+         RequestHandler::getValue(self::PARAMETER_FOO)
+      );
+
+      $_GET[self::PARAMETER_BAR] = '';
+      assertNull(
+         RequestHandler::getValue(self::PARAMETER_BAR, null, RequestHandler::USE_GET_PARAMS)
+      );
+
+      $_POST[self::PARAMETER_BAR] = '';
+      assertNull(
+         RequestHandler::getValue(self::PARAMETER_BAR, null, RequestHandler::USE_POST_PARAMS)
+      );
+
+      $_REQUEST[self::PARAMETER_BAR] = '';
+      assertNull(
+         RequestHandler::getValue(self::PARAMETER_BAR)
       );
    }
 
    public function testZeroValues() {
 
+      $_GET = array();
+
+      $_GET[self::PARAMETER_FOO] = 0;
+      assertEquals(0, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_GET_PARAMS));
+
+      $_GET[self::PARAMETER_BAR] = '0';
+      assertEquals('0', RequestHandler::getValue(self::PARAMETER_BAR, null, RequestHandler::USE_GET_PARAMS));
+
+      $_POST = array();
+
+      $_POST[self::PARAMETER_FOO] = 0;
+      assertEquals(0, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_POST_PARAMS));
+
+      $_POST[self::PARAMETER_BAR] = '0';
+      assertEquals('0', RequestHandler::getValue(self::PARAMETER_BAR, null, RequestHandler::USE_POST_PARAMS));
+
       $_REQUEST = array();
 
-      $_REQUEST['foo'] = 0;
-      assertEquals(0, RequestHandler::getValue('foo'));
+      $_REQUEST[self::PARAMETER_FOO] = 0;
+      assertEquals(0, RequestHandler::getValue(self::PARAMETER_FOO));
 
-      $_REQUEST['bar'] = '0';
-      assertEquals('0', RequestHandler::getValue('bar'));
+      $_REQUEST[self::PARAMETER_BAR] = '0';
+      assertEquals('0', RequestHandler::getValue(self::PARAMETER_BAR));
+
+   }
+
+   public function testParameterBoundaries() {
+
+      // separation of GET and POST - starting at GET
+      $_GET = array();
+      $_POST = array();
+      $_REQUEST = array();
+      $_GET[self::PARAMETER_FOO] = self::PARAMETER_BAR;
+      $_REQUEST[self::PARAMETER_FOO] = $_GET[self::PARAMETER_FOO];
+
+      assertEquals(self::PARAMETER_BAR, RequestHandler::getValue(self::PARAMETER_FOO));
+      assertEquals(self::PARAMETER_BAR, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_GET_PARAMS));
+      assertEquals(null, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_POST_PARAMS));
+
+      // separation of GET and POST - starting at POST
+      $_GET = array();
+      $_POST = array();
+      $_REQUEST = array();
+      $_POST[self::PARAMETER_FOO] = self::PARAMETER_BAR;
+      $_REQUEST[self::PARAMETER_FOO] = $_POST[self::PARAMETER_FOO];
+
+      assertEquals(self::PARAMETER_BAR, RequestHandler::getValue(self::PARAMETER_FOO));
+      assertEquals(self::PARAMETER_BAR, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_POST_PARAMS));
+      assertEquals(null, RequestHandler::getValue(self::PARAMETER_FOO, null, RequestHandler::USE_GET_PARAMS));
 
    }
 
