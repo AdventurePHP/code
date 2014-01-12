9607bed69913fad12a481a93991d38056172f617
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-01-12 21:25:54 +0000

    Added support for multi-line ServiceManager and DIServiceManager calls.

--- migration/migrate_sm_calls.php
+++ migration/migrate_sm_calls.php
@@ -3,34 +3,34 @@ include(dirname(__FILE__) . '/migrate_base.php');
 
 $files = filterApfDirectories(find('.', '*.php'));
 
-$search = '#\$this->getServiceObject\(\'([A-Za-z0-9:\-]+)\', ?\'([A-Za-z0-9_]+)\'\)#m';
-$searchWithType = '#\$this->getServiceObject\(\'([A-Za-z0-9:\-]+)\', ?\'([A-Za-z0-9_]+)\', ?([A-Za-z0-9:_]+)\)#m';
+$search = '#\$this->getServiceObject\(([ |\n|\r\n]*)\'([A-Za-z0-9:\-]+)\',([ |\n|\r\n]*)\'([A-Za-z0-9_]+)([ |\n|\r\n]*)\'\)#m';
+$searchWithType = '#\$this->getServiceObject\(([ |\n|\r\n]*)\'([A-Za-z0-9:\-]+)\',([ |\n|\r\n]*)\'([A-Za-z0-9_]+)\',([ |\n|\r\n]*)([A-Za-z0-9:_]+)([ |\n|\r\n]*)\)#m';
 
-$searchWithInit = '#\$this->getAndInitServiceObject\(\'([A-Za-z0-9:\-]+)\', ?\'([A-Za-z0-9_]+)\', ?(.+)\);#';
+$searchWithInit = '#\$this->getAndInitServiceObject\(([ |\n|\r\n]*)\'([A-Za-z0-9:\-]+)\',([ |\n|\r\n]*)\'([A-Za-z0-9_]+)\',([ |\n|\r\n]*)(.+)([ |\n|\r\n]*)\)#';
 
-$searchDi = '#\$this\->getDIServiceObject\(\'([A-Za-z0-9:\-]+)\', ?#m';
+$searchDi = '#\$this->getDIServiceObject\(([ |\n|\r\n]*)\'([A-Za-z0-9:\-]+)\',([ |\n|\r\n]*)(.+)([ |\n|\r\n]*)\)#m';
 
 foreach ($files as $file) {
    $content = file_get_contents($file);
 
    // replace single calls
    $content = preg_replace_callback($search, function ($matches) {
-      return '$this->getServiceObject(\'APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[2] . '\')';
+      return '$this->getServiceObject(\'APF\\' . str_replace('::', '\\', $matches[2]) . '\\' . $matches[4] . '\')';
    }, $content);
 
    // replace calls with service types
    $content = preg_replace_callback($searchWithType, function ($matches) {
-      return '$this->getServiceObject(\'APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[2] . '\', ' . $matches[3] . ')';
+      return '$this->getServiceObject(\'APF\\' . str_replace('::', '\\', $matches[2]) . '\\' . $matches[4] . '\', ' . $matches[6] . ')';
    }, $content);
 
    // replace calls with init calls
    $content = preg_replace_callback($searchWithInit, function ($matches) {
-      return '$this->getAndInitServiceObject(\'APF\\' . str_replace('::', '\\', $matches[1]) . '\\' . $matches[2] . '\', ' . $matches[3] . ');';
+      return '$this->getAndInitServiceObject(\'APF\\' . str_replace('::', '\\', $matches[2]) . '\\' . $matches[4] . '\', ' . $matches[6] . ')';
    }, $content);
 
    // replace DI calls
    $content = preg_replace_callback($searchDi, function ($matches) {
-      return '$this->getDIServiceObject(\'APF\\' . str_replace('::', '\\', $matches[1]) . '\', ';
+      return '$this->getDIServiceObject(\'APF\\' . str_replace('::', '\\', $matches[2]) . '\', ' . $matches[4] . ')';
    }, $content);
 
    // replace Singleton usages
