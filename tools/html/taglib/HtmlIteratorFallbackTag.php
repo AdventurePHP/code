b50753041e442d282bc83d57d92ad2b9bcb74e43
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-05-11 16:56:22 +0200

    ID#105: prevent fallback content to be transformed by accident within a controller.

--- tools/html/taglib/HtmlIteratorFallbackTag.php
+++ tools/html/taglib/HtmlIteratorFallbackTag.php
@@ -42,4 +42,9 @@ class HtmlIteratorFallbackTag extends TemplateTag {
       $this->tagLibs[] = new TagLib('APF\core\pagecontroller\LanguageLabelTag', 'fallback', 'getstring');
    }
 
+   public function transformOnPlace() {
+      // Do nothing (safety first!) to not allow manipulation (=display) of fallback
+      // content within controller. This has to be managed by the iterator itself!
+   }
+
 }
