69c392bf54597b66dabd312e09d103100803370f
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-05-30 07:33:34 +0200

    Optimized code to use APF API as much as possible.

--- tools/link/taglib/LinkGenerationTag.php
+++ tools/link/taglib/LinkGenerationTag.php
@@ -36,26 +36,31 @@ use APF\tools\link\Url;
  * Version 0.1, 28.07.2011<br />
  * Version 0.2, 22.11.2012 Werner Liemberger: removed a:getstring and ignored href bug<br />
  * Version 0.3, 15.05.2013 (Added use APF\tools\link\Url [Tobias Lückel|Megger])<br />
+ * Version 0.4, 30.06.2014 (Optimized code)<br />
  */
 class LinkGenerationTag extends Document {
 
+   const QUERY_OPTION_ATTRIBUTE_NAME = 'queryoption';
+   const HREF_ATTRIBUTE_NAME = 'href';
+
+   const QUERY_OPTION_OVERWRITE = 'set';
+   const QUERY_OPTION_MERGE = 'merge';
+
    public function transform() {
-      $parameters = $this->getAttributes();
 
-      if (isset($parameters['href'])) {
-         $url = Url::fromString($parameters['href']);
-         unset($parameters['href']);
-      } else {
+      $href = $this->getAttribute(self::HREF_ATTRIBUTE_NAME);
+      if ($href === null) {
          $url = Url::fromCurrent(true);
+      } else {
+         $url = Url::fromString($href);
+         $this->deleteAttribute(self::HREF_ATTRIBUTE_NAME);
       }
 
-      $queryOption = 'set';
-      if (isset($parameters['queryoption'])) {
-         $queryOption = $parameters['queryoption'];
-         unset($parameters['queryoption']);
-      }
+      $queryOption = $this->getAttribute(self::QUERY_OPTION_ATTRIBUTE_NAME, self::QUERY_OPTION_OVERWRITE);
+      $this->deleteAttribute(self::QUERY_OPTION_ATTRIBUTE_NAME);
 
-      if ($queryOption === 'merge') {
+      $parameters = $this->getAttributes();
+      if ($queryOption === self::QUERY_OPTION_MERGE) {
          $url->mergeQuery($parameters);
       } else {
          $url->setQuery($parameters);
