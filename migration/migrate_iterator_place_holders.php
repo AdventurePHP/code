fb56c58b56da35eed2d9a28914d170b554a19aff
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-07-22 20:58:44 +0200

    ID#218: added migration script part for iterator place holder with generic getter.

--- migration/migrate_iterator_place_holders.php
+++ migration/migrate_iterator_place_holders.php
@@ -5,10 +5,30 @@ $files = find('.', '*.html');
 
 $search = '#<html:placeholder([ |\n|\r\n|\r]+)getter ?= ?"(.+)"([ |\n|\r\n|\r]*)/>#U';
 
+$searchGenericGetter = '#<iterator:item([ |\n|\r\n|\r]+)getter ?= ?"(.+)">(.*)</iterator:item>#ms';
+$searchGenericGetterPlaceHolder = '#<html:placeholder([ |\n|\r\n|\r]+)name ?= ?"(.+)"([ |\n|\r\n|\r]*)/>#U';
+
 foreach ($files as $file) {
    $content = file_get_contents($file);
 
+   // migrate simple getter notation to extended templating syntax
    $content = preg_replace($search, '${item->\\2()}', $content);
+
+   // migrate generic getter notation to extended templating syntax as it is no longer supported
+   preg_match_all($searchGenericGetter, $content, $matches, PREG_SET_ORDER);
+
+   foreach ($matches as $match) {
+
+      $getter = $match[2];
+      $itemContent = $match[3];
+      preg_match_all($searchGenericGetterPlaceHolder, $itemContent, $innerMatches, PREG_SET_ORDER);
+
+      foreach ($innerMatches as $innerMatch) {
+         $itemContent = str_replace($innerMatch[0], '${item->' . $getter . '(\'' . $innerMatch[2] . '\')}', $itemContent);
+      }
+
+      $content = str_replace($match[0], '<iterator:item>' . $itemContent . '</iterator:item>', $content);
+   }
 
    file_put_contents($file, $content);
 }
