5ad133dccf628da8e37ce437ebc6ae8f2b0d7427
Christian Achatz <christian.achatz@adventure-php-framework.org>
2015-08-28 18:22:17 +0200

    ID#257: corrected migration script implementation to work with relative and absolute paths and on Win7 and Win10 via cygwin and bash.

--- migration/migrate-code.sh
+++ migration/migrate-code.sh
@@ -10,6 +10,11 @@ echo
 
 SCRIPT_DIR=$(dirname $0)
 
+case "$(uname -s)" in
+    CYGWIN*) PHP_SCRIPT_DIR=$(cygpath -m $SCRIPT_DIR) ;;
+    *) PHP_SCRIPT_DIR=$SCRIPT_DIR ;;
+esac
+
 # include checks and preparation
 source "$SCRIPT_DIR/check.sh"
 
@@ -20,37 +25,37 @@ echo "Starting migration ..."
 
 # migrate tag lib declaration
 echo "* Migrate taglib declaration statements ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_taglib_registration.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_taglib_registration.php
 
 echo "* Consolidate tag usage ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_consolidate_tag_usage.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_consolidate_tag_usage.php
 
 echo "* Remove redundant tag lib registration ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_extract_add_tag_statements.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_extract_add_tag_statements.php
 
 echo "* Switch to new place holder logic ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_update_place_holders_to_expression_notation.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_update_place_holders_to_expression_notation.php
 
 echo "* Migrate iterator item place holders ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_iterator_place_holders.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_iterator_place_holders.php
 
 echo "* Remove RequestHandler usage ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_remove_request_handler.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_remove_request_handler.php
 
 echo "* Remove HeaderManager usage ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_remove_header_manager.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_remove_header_manager.php
 
 echo "* Remove APF\tools\cookie\Cookie usage ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_remove_tools_cookie.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_remove_tools_cookie.php
 
 echo "* Migrate session handling to new API ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_session_handling.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_session_handling.php
 
 echo "* Migrate form definitions ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_form_definitions.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_form_definitions.php
 
 echo "* Migrate service object API ..."
-$PHP_BINARY $SCRIPT_DIR/migrate_service_object_api.php
+$PHP_BINARY $PHP_SCRIPT_DIR/migrate_service_object_api.php
 
 echo
 echo "#############################################"
