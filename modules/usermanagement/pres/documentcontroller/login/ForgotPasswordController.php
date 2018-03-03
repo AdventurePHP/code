01bcfd429d3c2447e3e729ee9df1e0869954e812
dave <david.mohrig@gmail.com>
2018-03-03 15:35:44 +0100

    ID#329, ID#330 Improvements
    2 small improvements for ForgotPasswordController as posted in
    APF-Tracker.

--- modules/usermanagement/pres/documentcontroller/login/ForgotPasswordController.php
+++ modules/usermanagement/pres/documentcontroller/login/ForgotPasswordController.php
@@ -22,6 +22,9 @@ namespace APF\modules\usermanagement\pres\documentcontroller\login;
 
 use APF\core\configuration\ConfigurationException;
 use APF\core\pagecontroller\BaseDocumentController;
+use APF\core\logging\LogEntry;
+use APF\core\logging\Logger;
+use APF\core\singleton\Singleton;
 use APF\modules\usermanagement\biz\UmgtManager;
 use APF\tools\form\validator\AbstractFormValidator;
 use APF\tools\link\LinkGenerator;
@@ -37,6 +40,7 @@ use Exception;
  * @version
  * Version 0.1, 28.10.2015
  * Version 0.2, 23.02.2016 (Added the possibility to use a forgotpw configuration file)
+ * Version 0.3, 26.02.2018 (Exception will be logged now, added configuration for reset URL)
  */
 class ForgotPasswordController extends BaseDocumentController {
 
@@ -74,26 +78,46 @@ class ForgotPasswordController extends BaseDocumentController {
                   throw new ConfigurationException('Section "' . $sectionName . '" is not defined within mailsender.ini. Please refer
                      the manual for more details.');
                }
-
+               
+               // get *.labels - configuration
                $labelConfig = $this->getConfiguration('APF\modules\usermanagement\pres', 'labels.ini');
                $subject = $labelConfig->getSection($this->getLanguage())->getValue('forgotpw.mail.subject');
                $content = $labelConfig->getSection($this->getLanguage())->getValue('forgotpw.mail.content');
-
-               // replace placeholders in text with username and link
-               $content = str_replace('{username}', $user->getUsername(), $content);
-               // try to get configuration file for hash-lifetime setting
+   
+               // try to get *.forgotpw - configuration or use default-settings
                try {
                   $forgetpwconfig = $this->getConfiguration('APF\modules\usermanagement\pres', 'forgotpw.ini');
                   $lifetime = $forgetpwconfig->getSection('Default')->getValue('hash.lifetime');
+                  $resetUrl = $forgetpwconfig->getSection('Default')->getValue('reset.url');
                } catch (ConfigurationException $e) {
                   $lifetime = '86400'; // default lifetime 24 hours
+                  $resetUrl = '';
                }
-               $content = str_replace('{lifetime}', $lifetime / 60 / 60, $content);
-               $link = LinkGenerator::generateUrl(Url::fromCurrent(true)
+
+               // replace placeholders in text with username and link
+               $content = str_replace('{username}', $user->getUsername(), $content);
+               $content = str_replace('{lifetime}', $lifetime / 60 / 60, $content);    // lifetime in hours
+               
+               if($resetUrl != '') {
+                  // get current Host and Scheme to generate new URL
+                  $currentUrl = Url::fromCurrent(true)->resetQuery();
+                  $host = $currentUrl->getHost();
+                  $scheme = $currentUrl->getScheme();
+                  $port = $currentUrl->getPort();
+                  
+                  // generate new URL with string from configuration-file
+                  $configUrl = Url::fromString($resetUrl)->setHost($host)->setScheme($scheme)
+                     ->setPort($port)->setQueryParameter('h', $passwordHash);
+                  $link = LinkGenerator::generateUrl($configUrl);
+               } else {
+                  // or generate default URL
+                  $link = LinkGenerator::generateUrl(Url::fromCurrent(true)
                      ->setQueryParameter('user', 'reset_pw')
                      ->setQueryParameter('h', $passwordHash));
+               }
+               
                $content = str_replace('{link}', $link, $content);
-
+               
                /* @var $builder MessageBuilder */
                $builder = $this->getServiceObject(MessageBuilder::class);
                $message = $builder->createMessage($sectionName, $subject, $content);
@@ -108,6 +132,10 @@ class ForgotPasswordController extends BaseDocumentController {
          } catch (Exception $e) {
             // system error message
             $this->getTemplate('system-error')->transformOnPlace();
+   
+            $l = Singleton::getInstance(Logger::class);
+            /* @var $l Logger */
+            $l->logEntry('login', 'Password cant be reset due to ' . $e, LogEntry::SEVERITY_ERROR);
          }
       } elseif ($form->isSent() && !$form->isValid()) {
          // error message
